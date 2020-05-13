f52f3e61ff5951cd7305bcb7ba06c77844435bc6
Richard <richytong@gmail.com>
2020-05-12 22:31:53 -0700

    nice webserver

--- examples/denoWebserverWithMiddleware.js
+++ examples/denoWebserverWithMiddleware.js
@@ -1,49 +1,57 @@
 import { serve } from 'https://deno.land/std/http/server.ts'
-import r, { pipe, map, transform, ternary, get, tap, eq } from '../mod.js'
+import {
+  pipe, fork, assign,
+  tap, tryCatch, switchCase,
+  map, filter, reduce, transform,
+  get, pick, omit,
+  any, all,
+  and, or, not,
+  eq, gt, lt, gte, lte,
+} from '../mod.js'
 
-const s = serve({ port: 8001 })
-console.log('http://localhost:8001/')
-
-const trace = tap(console.log)
+const join = delim => x => x.join(delim)
 
 const addServerTime = req => {
-  req.serverTime = Date.now()
+  req.serverTime = (new Date()).toJSON()
   return req
 }
 
-const formatTimestamp = ts => (new Date(ts)).toLocaleString()
+const trace = tap(console.log)
+
+const traceRequest = tap(pipe([
+  fork([
+    pipe([get('serverTime'), x => '[' + x + ']']),
+    get('method'),
+    get('url'),
+  ]),
+  join(' '),
+  trace,
+]))
 
 const respondWithHelloWorld = req => {
   req.respond({ body: 'Hello World\n' })
 }
 
 const respondWithServerTime = req => {
-  req.respond({ body: `The server time is ${formatTimestamp(req.serverTime)}` })
+  req.respond({ body: `The server time is ${req.serverTime}` })
 }
 
 const respondWithNotFound = req => {
   req.respond({ body: 'Not Found\n' })
 }
 
-/* TODO: uncomment this when done with switchCase and making operator functions binary
 const route = switchCase([
   eq('/', get('url')), respondWithHelloWorld,
   eq('/time', get('url')), respondWithServerTime,
-  eq(true, false), () => 'hey',
   respondWithNotFound,
 ])
-*/
-
-const route = req => {
-  if (req.url === '/') return respondWithHelloWorld(req)
-  if (req.url === '/time') return respondWithServerTime(req)
-  if (true === false) return 'hey'
-  return respondWithNotFound(req)
-}
 
 const onRequest = pipe([
   addServerTime,
+  traceRequest,
   route,
 ])
 
+const s = serve({ port: 8001 })
+console.log('http://localhost:8001/')
 transform(null, map(onRequest))(s)
