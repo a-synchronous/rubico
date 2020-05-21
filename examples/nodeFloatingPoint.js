91533d8dfe633135ae60a4188b8dd70fd7ba964b
Richard <richytong@gmail.com>
2020-05-21 14:24:36 -0700

    example: nodeFloatingPoint

---
+++ examples/nodeFloatingPoint.js
@@ -0,0 +1,51 @@
+const { map, tap, transform } = require('..')
+
+function SimpleQueue({ size }) {
+  this.size = size
+  this.buffer = []
+}
+
+SimpleQueue.prototype.push = function(item) {
+  this.buffer.push(item)
+  if (this.buffer.length > this.size) {
+    this.buffer.shift()
+  }
+  return this
+}
+
+SimpleQueue.prototype[Symbol.iterator] = function*() {
+  for (const item of this.buffer) {
+    yield item
+  }
+}
+
+const average = iterable => {
+  let sum = 0, count = 0
+  for (const item of iterable) {
+    sum += item
+    count += 1
+  }
+  return sum / count
+}
+
+const floatingPointAverage = ({ historySize }) => {
+  const queue = new SimpleQueue({ size: historySize })
+  return item => {
+    queue.push(item)
+    const avg = average(queue)
+    console.log(queue, avg)
+    return avg
+  }
+}
+
+const numbersStream = {
+  /* ... */
+  [Symbol.asyncIterator]: async function*() {
+    for (let i = 0; i < 1000; i++) yield i
+  },
+}
+
+transform(
+  map(floatingPointAverage({ historySize: 50 })),
+  null,
+)(numbersStream)
