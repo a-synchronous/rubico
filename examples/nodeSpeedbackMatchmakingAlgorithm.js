e9e2fbf765ce0ef2bfac2d376feca5e1864c645d
Richard <richytong@gmail.com>
2020-05-24 01:41:04 -0700

    nodeSpeedbackMatchmakingAlgorithm

---
+++ examples/nodeSpeedbackMatchmakingAlgorithm.js
@@ -0,0 +1,95 @@
+#!/usr/bin/env node
+
+const {
+  pipe, fork, tap, switchCase,
+  map, filter, transform,
+  and, not, eq, get,
+} = require('rubico')
+
+const identity = x => x
+
+const people = Array.from((function*() {
+  for (let i = 0; i < 6; i++) yield `${i + 1}`
+})())
+
+// [person] => Map { person => (Set { person }) }
+const makeTracker = transform(
+  map(fork([
+    person => person,
+    () => new Set(),
+  ])),
+  new Map(),
+)
+
+const db = new Map()
+
+const saveTracker = async tracker => { db.set('tracker', tracker) }
+
+const hasTracker = async () => db.has('tracker')
+
+const getTracker = async tracker => db.get('tracker')
+
+const isNotIn = set => x => !set.has(x)
+
+// { people, tracker } => { people, tracker, pairs }
+const algorithm = ({ people, tracker }) => {
+  const matchedThisRound = new Set()
+  return fork({
+    people: identity,
+    tracker: () => tracker,
+    pairs: transform(
+      pipe([
+        filter(isNotIn(matchedThisRound)),
+        map(person => {
+          for (const other of people) {
+            // console.log('person:other', `${person}:${other}`)
+            if (other === person) continue
+            if (tracker.get(person).has(other)) continue
+            if (matchedThisRound.has(other)) continue
+            tracker.get(person).add(other)
+            tracker.get(other).add(person)
+            matchedThisRound.add(person)
+            matchedThisRound.add(other)
+            return [person, other]
+          }
+        }),
+        filter(Array.isArray),
+      ]),
+      [],
+    )
+  })(people)
+}
+
+// [person, person, person, person, ...] => [[person, person], [person, person], ...]
+const matchmake = pipe([
+  fork({
+    people: identity,
+    tracker: switchCase([
+      hasTracker, getTracker,
+      makeTracker,
+    ]),
+  }),
+  algorithm,
+  tap(pipe([get('tracker'), saveTracker])),
+])
+
+const didEveryoneMatch = (tracker, people) => {
+  for (const [, set] of tracker) {
+    if (set.size < people.length - 1) return false // 1 for yourself
+  }
+  return true
+}
+
+const main = async () => {
+  console.log('people', people)
+  let i = 0
+  while (true) {
+    const round = await matchmake(people)
+    const { tracker, pairs } = round
+    console.log('round', i, pairs)
+    if (didEveryoneMatch(tracker, people)) break
+    i += 1
+  }
+}
+
+main()
