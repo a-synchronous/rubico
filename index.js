90a57cb06211809317c6ea6f75e2cf17bcbafa42
Richard <richytong@gmail.com>
2020-05-29 09:43:42 -0700

    global rubico for script imports

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (global = global || self, factory(global.rubico = {}));
+}(this, function (exports) {
+
 'use strict'
 
 /* design principles
@@ -1059,3 +1065,5 @@ const r = {
 }
 
 module.exports = r
+
+}))
