1cc3aa1f75e070dc61c89221888721a1406e4ca6
Richard <richytong@gmail.com>
2020-07-17 11:48:04 -0700

    prune forEach

--- x/forEach.js
+++ x/forEach.js
@@ -1,9 +1,5 @@
 const { map, tap } = require('..')
 
-const isFunction = x => typeof x === 'function'
-
-// const forEach = f => x => isFunction(x) ? tap(x) : tap(map(f))(x)
-
 const forEach = f => map(tap(f))
 
 module.exports = forEach
