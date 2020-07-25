e7b3f15463453ccf2e8f62e04aeeaa5a5ed1fb6a
Richard <richytong@gmail.com>
2020-07-24 22:13:36 -0700

    test loop 0 timeInLoop

--- x/timeInLoop.test.js
+++ x/timeInLoop.test.js
@@ -4,7 +4,14 @@ const timeInLoop = require('./timeInLoop')
 describe('timeInLoop', () => {
   it('times the amount of complete synchronous function calls in a loop', async () => {
     assert.strictEqual(
-      typeof timeInLoop('myFn', 1e6, () => 'yo'),
+      typeof timeInLoop(1e6, () => 'yo'),
+      'number',
+    )
+  })
+
+  it('0 for 0', async () => {
+    assert.strictEqual(
+      typeof timeInLoop(0, () => 'yo'),
       'number',
     )
   })
