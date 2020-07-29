fd9d4000a9bbc6f74a707aa8c9f55e316564ca92
Richard <richytong@gmail.com>
2020-07-29 15:41:31 -0700

    nice benchmarks bro

--- benchmarks/switchCase.js
+++ benchmarks/switchCase.js
@@ -1,59 +1,24 @@
 const { switchCase } = require('..')
-const timeInLoop = require('../x/timeInLoop')
-
-const numLoops = 1e6
+const { timeInLoop } = require('../x')
 
 const isOdd = x => x % 2 === 1
 
-describe('switchCase', () => {
-  describe('switchCase([isOdd, () => \'odd\', () => \'even\']); timeInLoop', () => {
-    it('switchCase', async () => {
-      console.log(
-        timeInLoop(
-          numLoops,
-          switchCase([isOdd, () => 'odd', () => 'even']),
-        ) + 'ms',
-      )
-    })
-    it('switchCase (fresh scopes)', async () => {
-      console.log(
-        timeInLoop(
-          numLoops,
-          () => switchCase([isOdd, () => 'odd', () => 'even'])(),
-        ) + 'ms',
-      )
-    })
-    it('conditional operator cond ? a : b', async () => {
-      console.log(
-        timeInLoop(
-          numLoops,
-          x => isOdd(x) ? 1 : 0,
-        ) + 'ms',
-      )
-    })
-    it('if else statement', async () => {
-      console.log(
-        timeInLoop(
-          numLoops,
-          x => {
-            if (isOdd(x)) return 'odd'
-            else return 'even'
-          },
-        ) + 'ms',
-      )
-    })
-    it('switch case statement', async () => {
-      console.log(
-        timeInLoop(
-          numLoops,
-          x => {
-            switch (isOdd(x)) {
-              case true: return 'odd'
-              default: return 'even'
-            }
-          },
-        ) + 'ms',
-      )
-    })
-  })
+timeInLoop("isOdd", 1e6, () => isOdd(4))
+
+timeInLoop("isOdd_ternary", 1e6, () => isOdd(4) ? 1 : 0)
+
+timeInLoop("isOdd_ifElse", 1e6, () => {
+  if (isOdd(4)) return 'odd'
+  else return 'even'
 })
+
+timeInLoop("isOdd_switch", 1e6, () => {
+  switch (isOdd(4)) {
+    case true: return 'odd'
+    default: return 'even'
+  }
+})
+
+const oddOrEven = switchCase([isOdd, () => 'odd', () => 'even'])
+
+timeInLoop("isOdd_rubicoSwitchCase", 1e6, () => oddOrEven(4))
