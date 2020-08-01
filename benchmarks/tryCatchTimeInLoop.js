3b44d8ca8ae9af7eefed0495f5b20ca082eb90af
Richard <richytong@gmail.com>
2020-08-01 10:57:27 -0700

    add Bo5 tryCatch

--- benchmarks/tryCatchTimeInLoop.js
+++ benchmarks/tryCatchTimeInLoop.js
@@ -9,6 +9,7 @@ timeInLoop('noop', 1e5, () => {
 
 const throwError = err => { throw err }
 
+// Bo5: 771.797ms
 timeInLoop('tryCatch_vanilla', 1e5, () => {
   try {
     throwError(new Error('hey'))
@@ -19,10 +20,12 @@ timeInLoop('tryCatch_vanilla', 1e5, () => {
 
 const runner = tryCatch(throwError, noop)
 
+// Bo5: 764.897ms
 timeInLoop('tryCatch_rubicoHappyPath', 1e5, () => {
   runner(new Error('hey'))
 })
 
+// Bo5: 783.742ms
 timeInLoop('tryCatch_rubicoIncludingInit', 1e5, () => {
   tryCatch(throwError, noop)(new Error('hey'))
 })
