07579d840791363337244dee250472e0818181e1
Richard <richytong@gmail.com>
2020-08-01 11:06:48 -0700

    switchCase benchmark reports

--- benchmarks/switchCaseTimeInLoop.js
+++ benchmarks/switchCaseTimeInLoop.js
@@ -3,15 +3,19 @@ const { timeInLoop } = require('../x')
 
 const isOdd = x => x % 2 === 1
 
+// 4.448ms
 timeInLoop("isOdd", 1e6, () => isOdd(4))
 
+// 10.631ms
 timeInLoop("isOdd_ternary", 1e6, () => isOdd(4) ? 1 : 0)
 
+// 9.585ms
 timeInLoop("isOdd_ifElse", 1e6, () => {
   if (isOdd(4)) return 'odd'
   else return 'even'
 })
 
+// 10.137ms
 timeInLoop("isOdd_switch", 1e6, () => {
   switch (isOdd(4)) {
     case true: return 'odd'
@@ -21,4 +25,10 @@ timeInLoop("isOdd_switch", 1e6, () => {
 
 const oddOrEven = switchCase([isOdd, () => 'odd', () => 'even'])
 
+// 24.529ms
 timeInLoop("isOdd_rubicoSwitchCase", 1e6, () => oddOrEven(4))
+
+// 127.702ms
+timeInLoop("isOdd_rubicoIncludingInit", 1e6, () => {
+  switchCase([isOdd, () => 'odd', () => 'even'])(4)
+})
