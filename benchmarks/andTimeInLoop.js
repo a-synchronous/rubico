97b30d778e54e68ae649f443f3bae0729f56d3f2
Richard <richytong@gmail.com>
2020-08-01 11:01:24 -0700

    and - needs work

--- benchmarks/andTimeInLoop.js
+++ benchmarks/andTimeInLoop.js
@@ -3,18 +3,21 @@ const timeInLoop = require('../x/timeInLoop')
 
 const isOdd = x => x % 2 === 1
 
+// 4.326ms
 timeInLoop('isOdd', 1e6, () => {
   isOdd(9)
 })
 
 const gt2 = x => x > 2
 
+// 10.47ms
 timeInLoop('gt2', 1e6, () => {
   gt2(9)
 })
 
 const isOddGt2 = x => isOdd(x) && gt2(x)
 
+// 10.458ms
 timeInLoop('x => isOdd(x) && gt2(x)', 1e6, () => {
   isOddGt2(9)
 })
@@ -24,15 +27,15 @@ const testRubicoAnd = and([
   x => x > 2,
 ])
 
-timeInLoop('and([x => x % 2 === 1, x => x > 2])', 1e6, () => {
+// 68.633ms
+timeInLoop('and_rubicoHappyPath', 1e6, () => {
   testRubicoAnd(9)
 })
 
-const testRubicoAnd2 = and([
-  isOdd,
-  gt2,
-])
-
-timeInLoop('and([isOdd, greaterThan2])', 1e6, () => {
-  testRubicoAnd2(9)
+// 153.602ms
+timeInLoop('and_rubicoIncludingInit', 1e6, () => {
+  and([
+    x => x % 2 === 1,
+    x => x > 2,
+  ])(9)
 })
