69241414121d7e1ed10f780129eee89ac5b1cc96
Richard <richytong@gmail.com>
2020-08-02 14:02:36 -0700

    json?

--- index.js
+++ index.js
@@ -152,6 +152,9 @@ const reverseArrayIter = arr => (function*() {
 })()
 
 /*
+ * @name
+ * pipe
+ *
  * @synopsis
  * pipe(
  *   funcs Array<function>,
@@ -161,13 +164,15 @@ const reverseArrayIter = arr => (function*() {
  *   funcs Array<function>,
  * )(reducer function) -> composedReducer function
  *
+ * @description
+ * `pipe` is a function composition function that takes an Array of functions `funcs` and returns an anonymous inner function `pipe(funcs)`. `pipe(funcs)` accepts any number of arguments `args` and supplies them to the first function of `funcs`. The result of that call is supplied as a single argument to the next function, and so on until all functions of `funcs` have been called. The return value of `pipe(funcs)` for a given input is the return value of the final function of the array of functions `funcs` in a chain.
+ *
+ * When `pipe(funcs)` is passed a `reducer` function, the returned result is another reducer function `composedReducer` that would perform the pipeline operation described by `funcs` on every element of a given collection when used with [reduce](https://doc.rubico.land/#reduce) or the `.reduce` method of an Array. For more information on this behavior, please see [transducers](https://github.com/a-synchronous/rubico/blob/master/TRANSDUCERS.md)
+ *
  * @catchphrase
  * define flow: chain functions together
  *
- * @description
- * `pipe` is a higher order function that takes an Array of functions `funcs` and returns an anonymous inner function `pipe(funcs)`. `pipe(funcs)` accepts any number of arguments `args` and supplies them to the first function of `funcs`. The result of that call is supplied as a single argument to the next function, and so on until all functions of `funcs` have been called. The return value of `pipe(funcs)` for a given input is the return value of the final function of the array of functions `funcs` in a chain.
- *
- * When `pipe(funcs)` is passed a `reducer` function, the returned result is another reducer function `composedReducer` that would perform the pipeline operation described by `funcs` on every element of a given collection when used with [reduce](https://doc.rubico.land/#reduce) or the `.reduce` method of an Array. For more information on this behavior, please see [transducers](https://github.com/a-synchronous/rubico/blob/master/TRANSDUCERS.md)
+ * @concurrency: series
  *
  * @TODO: refactor to PossiblePromise.args
  */
@@ -230,7 +235,7 @@ const objectFork = (fns, x) => {
  * )(x Promise<T>|T) -> y Promise<Array>|Array
  *
  * @description
- * `fork`
+ * `fork` is a function composition and concurrent execution function.
  */
 const fork = fns => {
   if (isArray(fns)) {
