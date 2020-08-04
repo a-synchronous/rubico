55d2b830037c56192c9272e219d620c6c38833c8
Richard <richytong@gmail.com>
2020-08-04 16:48:52 -0700

    report results

--- x/find.benchmark.js
+++ x/find.benchmark.js
@@ -4,38 +4,43 @@ const R = require('ramda')
 
 const gt0 = x => x > 0
 
+// 3.607ms
 timeInLoop('gt0', 1e6, () => {
   gt0(1)
 })
 
 const array = [1, 2, 3]
 
+// 12.539ms
 timeInLoop('gt0_Array.prototype.find', 1e6, () => {
   array.find(gt0)
 })
 
 const findHappy = find(gt0)
 
+// 86.023ms
 timeInLoop('gt0_findHappyPath', 1e6, () => {
   findHappy(array)
 })
 
-const findHappyRamda = R.find(gt0)
-
+// 101.791ms
 timeInLoop('gt0_findRamdaHappyPath', 1e6, () => {
-  findHappyRamda(array)
+  R.find(gt0, array)
 })
 
+// 145.28ms
 timeInLoop('gt0_findFullInit', 1e6, () => {
   find(gt0)(array)
 })
 
+// 375.694ms
 timeInLoop('gt0_findRamdaFullInit', 1e6, () => {
   R.find(gt0)(array)
 })
 
 const object = { a: 1, b: 2, c: 3 }
 
+// 654.661ms
 timeInLoop('gt0_findObjectHappyPath', 1e6, () => {
   find(gt0)(object)
 })
