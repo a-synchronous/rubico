56d426219d5d9299f74fb2646200f2c11890e6f1
Richard <richytong@gmail.com>
2020-08-04 15:26:17 -0700

    update possible-promise license to just rubico

--- monad/possible-promise.js
+++ monad/possible-promise.js
@@ -1,5 +1,5 @@
 /* rubico v1.5.0
- * https://github.com/a-synchronous/rubico/monad/possible-promise
+ * https://github.com/a-synchronous/rubico
  * (c) 2019-2020 Richard Tong
  * rubico may be freely distributed under the MIT license.
  */
