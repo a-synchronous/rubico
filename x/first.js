f7c388e38aa548cc7f2471517b06ced89c460015
Richard <richytong@gmail.com>
2020-08-04 22:19:57 -0700

    use more Orderable for first

--- x/first.js
+++ x/first.js
@@ -3,12 +3,42 @@ const isString = require('./isString')
 
 /*
  * @synopsis
+ * new Orderable(x string|Array) -> Orderable
+ */
+const Orderable = function(x) {
+  this.value = x
+}
+
+/*
+ * @synopsis
+ * Orderable.isOrderable(x string|Array) -> boolean
+ */
+Orderable.isOrderable = x => Array.isArray(x) || isString(x)
+
+/*
+ * @synopsis
+ * new Orderable(x string).item(i number) -> string
+ *
+ * new Orderable(x Array).item(i number) -> any
+ */
+Orderable.prototype.item = function(i) {
+  return this.value[i]
+}
+
+/*
+ * @name
+ * first
+ *
+ * @synopsis
  * first(x Promise<string>|string) -> firstChar string
  *
  * first(x Promise<Array>|Array) -> firstItem any
+ *
+ * @catchphrase
+ * Get the first item from an orderable collection
  */
 const first = PossiblePromise.args(x => {
-  if (Array.isArray(x) || isString(x)) return x[0]
+  if (Orderable.isOrderable(x)) return new Orderable(x).item(0)
   throw new TypeError('first(x); x is not an Array or String')
 })
 
