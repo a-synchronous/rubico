14cb5b255d76b960bc1830e851935a7d6469af11
Richard <richytong@gmail.com>
2020-08-04 22:05:27 -0700

    first PossiblePromise revamp

--- x/first.js
+++ x/first.js
@@ -1,12 +1,15 @@
-const { or, get } = require('..')
-const is = require('./is')
+const PossiblePromise = require('../monad/possible-promise')
 const isString = require('./isString')
 
-const first = x => {
-  if (!is(Array)(x) && !isString(x)) {
-    throw new TypeError('first(x); x is not an Array or String')
-  }
-  return get(0)(x)
-}
+/*
+ * @synopsis
+ * first(x Promise<string>|string) -> firstChar string
+ *
+ * first(x Promise<Array>|Array) -> firstItem any
+ */
+const first = PossiblePromise.args(x => {
+  if (Array.isArray(x) || isString(x)) return x[0]
+  throw new TypeError('first(x); x is not an Array or String')
+})
 
 module.exports = first
