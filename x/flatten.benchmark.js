e1f9b57a077cdcb4433ae0f0db28b93af0d0d3e1
Richard <richytong@gmail.com>
2020-08-05 16:01:00 -0700

    flatten benchmark report

--- x/flatten.benchmark.js
+++ x/flatten.benchmark.js
@@ -5,23 +5,22 @@ const _ = require('lodash')
 
 const nested = [[1], 2, [[3]]]
 
-console.log(nested.flat(1))
-console.log(flatten(nested))
-console.log(R.unnest(nested))
-console.log(_.flatten(nested))
-
+// 126.669ms
 timeInLoop('[...].flat(1)', 1e5, () => {
   nested.flat(1)
 })
 
+// 32.433ms
 timeInLoop('rubico flatten', 1e5, () => {
   flatten(nested)
 })
 
+// 143.928ms
 timeInLoop('R.unnest', 1e5, () => {
   R.unnest(nested)
 })
 
+// 20.189ms
 timeInLoop('_.flatten', 1e5, () => {
   _.flatten(nested)
 })
