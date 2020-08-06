70956292a1ed88588e66ee3b15e0b384cda3ab57
Richard <richytong@gmail.com>
2020-08-06 09:12:30 -0700

    refactored Flattenable

--- x/flatten.benchmark.js
+++ x/flatten.benchmark.js
@@ -5,22 +5,14 @@ const _ = require('lodash')
 
 const nested = [[1], 2, [[3]]]
 
-// 126.669ms
-timeInLoop('[...].flat(1)', 1e5, () => {
-  nested.flat(1)
-})
+// 119.954ms
+timeInLoop('[...].flat(1)', 1e5, () => { nested.flat(1) })
 
-// 32.433ms
-timeInLoop('rubico flatten', 1e5, () => {
-  flatten(nested)
-})
+// 37.223ms
+timeInLoop('rubico.x.flatten', 1e5, () => { flatten(nested) })
 
-// 143.928ms
-timeInLoop('R.unnest', 1e5, () => {
-  R.unnest(nested)
-})
+// 148.861ms
+timeInLoop('R.unnest', 1e5, () => { R.unnest(nested) })
 
-// 20.189ms
-timeInLoop('_.flatten', 1e5, () => {
-  _.flatten(nested)
-})
+// 28.193ms
+timeInLoop('_.flatten', 1e5, () => { _.flatten(nested) })
