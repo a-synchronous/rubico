d1e037dd76ce7e66ab2538c560b2418ee6acf183
Richard <richytong@gmail.com>
2020-08-08 12:07:27 -0700

    new benchmark format for flatten

--- x/flatten.benchmark.js
+++ x/flatten.benchmark.js
@@ -3,16 +3,22 @@ const flatten = require('./flatten')
 const R = require('ramda')
 const _ = require('lodash')
 
+/**
+ * @name flatten
+ *
+ * @benchmark
+ * [...].flat(1): 1e+5: 118.159ms
+ * flatten: 1e+5: 29.453ms
+ * R.unnest: 1e+5: 150.123ms
+ * _.flatten: 1e+5: 28.312ms
+ */
+
 const nested = [[1], 2, [[3]]]
 
-// 119.954ms
-timeInLoop('[...].flat(1)', 1e5, () => { nested.flat(1) })
+// timeInLoop('[...].flat(1)', 1e5, () => { nested.flat(1) })
 
-// 37.223ms
-timeInLoop('rubico.x.flatten', 1e5, () => { flatten(nested) })
+// timeInLoop('flatten', 1e5, () => { flatten(nested) })
 
-// 148.861ms
-timeInLoop('R.unnest', 1e5, () => { R.unnest(nested) })
+// timeInLoop('R.unnest', 1e5, () => { R.unnest(nested) })
 
-// 28.193ms
 timeInLoop('_.flatten', 1e5, () => { _.flatten(nested) })
