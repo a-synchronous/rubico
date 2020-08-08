2993656ae1fc4a43370d1521ffa229d593829c9e
Richard <richytong@gmail.com>
2020-08-07 20:01:45 -0700

    first revamp

--- x/first.js
+++ x/first.js
@@ -1,30 +1,11 @@
 const PossiblePromise = require('../monad/PossiblePromise')
 const Instance = require('../monad/Instance')
 
-/*
- * @synopsis
- * new Orderable(x string|Array) -> Orderable
- */
-const Orderable = function(x) {
-  this.value = x
-}
+const { isArray, isString } = Instance
 
-/*
- * @synopsis
- * Orderable.isOrderable(x string|Array) -> boolean
- */
-Orderable.isOrderable = x => (Instance.isInstance(x)
-  && (Instance.isArray(x) || Instance.isString(x)))
+const isOrderable = x => isArray(x) || isString(x)
 
-/*
- * @synopsis
- * new Orderable(x string).item(i number) -> string
- *
- * new Orderable(x Array).item(i number) -> any
- */
-Orderable.prototype.item = function(i) {
-  return this.value[i]
-}
+const possiblePromiseArgs = PossiblePromise.args
 
 /*
  * @name
@@ -38,8 +19,8 @@ Orderable.prototype.item = function(i) {
  * @catchphrase
  * Get the first item from an orderable collection
  */
-const first = PossiblePromise.args(x => {
-  if (Orderable.isOrderable(x)) return new Orderable(x).item(0)
+const first = possiblePromiseArgs(x => {
+  if (isOrderable(x)) return x[0]
   throw new TypeError('first(x); x is not an Array or String')
 })
 
