d0196c1005592bbb2440e7114f484952b17b32ac
Richard <richytong@gmail.com>
2020-08-08 17:48:59 -0700

    refresh find benchmarks

--- x/find.benchmark.js
+++ x/find.benchmark.js
@@ -3,34 +3,29 @@ const find = require('./find')
 const R = require('ramda')
 const _ = require('lodash')
 
+/**
+ * @name find
+ *
+ * @benchmark
+ * gt0: 1e+7: 12.394ms
+ * gt0_Array.prototype.find: 1e+7: 32.779ms
+ * gt0_findHappyPath: 1e+7: 337.765ms
+ * gt0_findRamdaHappyPath: 1e+7: 787.56ms
+ * gt0_findLodashHappyPath: 1e+7: 2.874s
+ */
+
 const gt0 = x => x > 0
 
-// 3.636ms
-timeInLoop('gt0', 1e6, () => { gt0(1) })
+// timeInLoop('gt0', 1e7, () => { gt0(1) })
 
 const array = [1, 2, 3]
 
-// 6.417ms
-timeInLoop('gt0_Array.prototype.find', 1e6, () => { array.find(gt0) })
+// timeInLoop('gt0_Array.prototype.find', 1e7, () => { array.find(gt0) })
 
 const findHappy = find(gt0)
 
-// 55.018ms
-timeInLoop('gt0_findHappyPath', 1e6, () => { findHappy(array) })
+// timeInLoop('gt0_find', 1e7, () => { findHappy(array) })
 
-// 93.053ms
-timeInLoop('gt0_findRamdaHappyPath', 1e6, () => { R.find(gt0, array) })
+// timeInLoop('gt0_findRamdaHappyPath', 1e7, () => { R.find(gt0, array) })
 
-// 323.106ms
-timeInLoop('gt0_findLodashHappyPath', 1e6, () => { _.find(gt0, array) })
-
-// 153.906ms
-timeInLoop('gt0_findFullInit', 1e6, () => { find(gt0)(array) })
-
-// 365.532ms
-timeInLoop('gt0_findRamdaFullInit', 1e6, () => { R.find(gt0)(array) })
-
-const object = { a: 1, b: 2, c: 3 }
-
-// 262.711ms
-timeInLoop('gt0_findObjectHappyPath', 1e6, () => { find(gt0)(object) })
+// timeInLoop('gt0_findLodashHappyPath', 1e7, () => { _.find(gt0, array) })
