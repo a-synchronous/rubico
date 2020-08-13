31bec23bdf574f096283fe0cedb09a6751662cb7
Richard <richytong@gmail.com>
2020-08-13 14:10:27 -0700

    add uniq inline doc entry

--- x/uniq.js
+++ x/uniq.js
@@ -4,6 +4,23 @@ const { isArray } = Instance;
 
 const { reduce } = require('../index');
 
+/**
+ * @name uniq
+ *
+ * @synopsis
+ * <T any>uniq(arr Array<T>) -> uniqArr Array<T>
+ *
+ * @catchphrase
+ * Get unique values of a collection
+ *
+ * @description
+ * `uniq` takes an Array of items and returns an Array of items containing only the unique items of that Array.
+ *
+ * @example
+ * console.log(
+ *   uniq([1, 1, 2, 2, 3, 3, 3]),
+ * ) // [1, 2, 3]
+ */
 const uniq = arr => {
   if (!isArray(arr)) throw Error('uniq(arr): arr is not an array');
 
