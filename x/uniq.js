29d14bd9106dd6e23822b5756ad0662f7becdd68
Frederic Heem <frederic.heem@gmail.com>
2020-08-13 07:38:03 +0200

    uniq

---
+++ x/uniq.js
@@ -0,0 +1,22 @@
+const Instance = require('../monad/Instance');
+
+const { isArray } = Instance;
+
+const { reduce } = require('../index');
+
+const uniq = (arr) => {
+  if (!isArray(arr)) {
+    throw Error('uniq(arr): arr is not an array');
+  }
+  const seenSet = new Set();
+  return reduce((acc, value) => {
+    if (seenSet.has(value)) {
+      return acc;
+    } else {
+      seenSet.add(value);
+      return [...acc, value];
+    }
+  }, [])(arr);
+};
+
+module.exports = uniq;
