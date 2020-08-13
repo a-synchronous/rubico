2b809a9bcf5f6a9cea3a86e89ddbae1050156ab6
Frederic Heem <frederic.heem@gmail.com>
2020-08-13 07:48:48 +0200

    got bitten again by the formatter

--- x/uniq.js
+++ x/uniq.js
@@ -4,14 +4,14 @@ const { isArray } = Instance;
 
 const { reduce } = require('../index');
 
-const uniq = (arr) => {
+const uniq = arr => {
   if (!isArray(arr)) throw Error('uniq(arr): arr is not an array');
-  
+
   const seenSet = new Set();
   return reduce((acc, value) => {
     if (seenSet.has(value)) {
       return acc;
-    } 
+    }
     seenSet.add(value);
     return [...acc, value];
   }, [])(arr);
