78d1ded2bee277fddfef879f769800afda3a9b4f
Richard <richytong@gmail.com>
2020-08-13 12:11:56 -0700

    unionWith

--- x/unionWith.js
+++ x/unionWith.js
@@ -1,36 +1,244 @@
+const PossiblePromise = require('../monad/PossiblePromise')
+const Instance = require('../monad/Instance')
+const Mux = require('../monad/Mux')
 const { switchCase, reduce, flatMap, any } = require('..')
 const is = require('./is')
 
-const isFunction = x => typeof x === 'function'
+const { isArray, isSet, isFunction, isPromise } = Instance
 
-const isPromise = x => x && typeof x.then === 'function'
+const symbolIterator = Symbol.iterator
 
-const concat = (y, yi) => (isPromise(yi)
-  ? yi.then(res => y.concat(res))
-  : y.concat(yi))
+const symbolAsyncIterator = Symbol.asyncIterator
 
-const arrayUnionWith = (binaryPredicate, x) => reduce(
-  (y, xi) => concat(y, flatMap(
-    switchCase([
-      xii => any(
-        yi => binaryPredicate(yi, xii)
-      )(y), () => [],
-      xi => [xi],
-    ]),
-  )(xi)),
-  () => new x.constructor(),
-)(x)
+const possiblePromiseThen = PossiblePromise.then
 
-const unionWith = binaryPredicate => {
-  if (!isFunction(binaryPredicate) || binaryPredicate.length !== 2) {
-    throw new TypeError('unionWith(f); f is not a binary predicate function')
+const {
+  isAsyncSequence,
+  concat: muxConcat,
+} = Mux
+
+/**
+ * @name arrayPush
+ *
+ * @synopsis
+ * arrayPush(arr Array, value any) -> undefined
+ */
+const arrayPush = (arr, value) => {
+  arr[arr.length] = value
+}
+
+/**
+ * @name setAdd
+ *
+ * @synopsis
+ * setAdd(set Set, value any) -> undefined
+ */
+const setAdd = (set, value) => {
+  set.add(value)
+}
+
+/**
+ * @name asyncExistsWith
+ *
+ * @synopsis
+ * <T any>asyncExistsWith(
+ *   predicate (T, T)=>Promise<boolean>,
+ *   value T,
+ *   iter Iterable<T>,
+ * ) -> doesValueExistInResultByPredicate Promise<boolean>
+ */
+const asyncExistsWith = async (predicate, member, iter) => {
+  for (const item of iter) {
+    if (await predicate(item, member)) return true
   }
-  return x => {
-    if (!is(Array)(x)) {
-      throw new TypeError('unionWith(...)(x); x is not an Array')
+  return false
+}
+
+/**
+ * @name existsWith
+ *
+ * @synopsis
+ * <T any>existsWith(
+ *   predicate (T, T)=>boolean,
+ *   member T,
+ *   iter Iterable<T>,
+ * ) -> doesValueExistInResultByPredicate boolean
+ */
+const existsWith = (predicate, member, iter) => {
+  const iterNext = iter.next.bind(iter)
+  while (true) {
+    const { value, done } = iterNext()
+    if (done) return false
+    const exists = predicate(member, value)
+    if (isPromise(exists)) return exists.then(res => (
+      res ? true : asyncExistsWith(predicate, member, iter)))
+    if (exists) return true
+  }
+}
+
+/**
+ * @name asyncGenericUnionWithGeneric
+ *
+ * @synopsis
+ * <T any>asyncGenericUnionWithGeneric(
+ *   predicate (T, T)=>boolean,
+ *   iter Iterator<T>,
+ *   result Iterable<T>,
+ *   setter (result Iterable<T>, T)=>(),
+ * ) -> result Promise|Iterable<T>
+ */
+const asyncGenericUnionWithGeneric = async (predicate, iter, result, setter) => {
+  const iterNext = iter.next.bind(iter)
+  while (true) {
+    const { value, done } = await iterNext()
+    if (done) return result
+    if (await existsWith(predicate, value, result[symbolIterator]())) continue
+    setter(result, value)
+  }
+}
+
+/**
+ * @name genericUnionWithGeneric
+ *
+ * @synopsis
+ * <T any>genericUnionWithGeneric(
+ *   predicate (T, T)=>boolean,
+ *   iter Iterator<T>,
+ *   result Iterable<T>,
+ *   setter (result Iterable<T>, T)=>(),
+ * ) -> result Promise|Iterable<T>
+ */
+const genericUnionWithGeneric = (predicate, iter, result, setter) => {
+  const iterNext = iter.next.bind(iter)
+  const next = iterNext()
+  if (isPromise(next)) return next.then(({ value, done }) => {
+    if (done) return result
+    setter(result, value)
+    return asyncGenericUnionWithGeneric(predicate, iter, result, setter)
+  })
+  const { value, done } = next
+  if (done) return result
+  setter(result, value)
+  while (true) {
+    const { value, done } = iterNext()
+    if (done) return result
+    const exists = existsWith(predicate, value, result[symbolIterator]())
+    if (isPromise(exists)) return exists.then(res => {
+      if (!res) setter(result, value)
+      return asyncGenericUnionWithGeneric(predicate, iter, result, setter)
+    })
+    if (exists) continue
+    setter(result, value)
+  }
+}
+
+/* const iteratorUnionWithArray = (predicate, iter) => {
+  const result = []
+  for (const value of iter) {
+    const exists = existsWith(predicate, value, result)
+    if (isPromise(exists)) {
+      const iterCopy = [...iter][symbolIterator]() // TODO: investigate this workaround. iter is empty inside the .then block
+                                                   // there may be a bug with for const of
+      return exists.then(res => {
+        if (!res) arrayPush(result, value)
+        return asyncIteratorUnionWithArray(predicate, iterCopy, result)
+      })
     }
-    return arrayUnionWith(binaryPredicate, x)
+    if (exists) continue
+    arrayPush(result, value)
   }
+  return result
+} */
+
+/**
+ * @name iteratorUnionWithIterator
+ *
+ * @synopsis
+ * <T any>iteratorUnionWithIterator(
+ *   predicate (T, T)=>boolean
+ *   iter Iterator<T>
+ * ) -> Iterator<T>
+ */
+const iteratorUnionWithIterator = function*(predicate, iter) {
+  const yielded = [], iterNext = iter.next.bind(iter)
+  while (true) {
+    const { value, done } = iterNext()
+    if (done) return
+    if (existsWith(predicate, value, yielded[symbolIterator]())) continue
+    arrayPush(yielded, value)
+    yield value
+  }
+  /*
+  for (const value of iter) {
+    if (existsWith(predicate, value, yielded[symbolIterator]())) continue
+    arrayPush(yielded, value)
+    yield value
+  }
+  */
+}
+
+/**
+ * @name asyncIteratorUnionWithAsyncIterator
+ *
+ * @synopsis
+ * <T any>asyncIteratorUnionWithAsyncIterator(
+ *   predicate (T, T)=>boolean
+ *   iter AsyncIterator<T>
+ * ) -> AsyncIterator<T>
+ */
+const asyncIteratorUnionWithAsyncIterator = async function*(predicate, iter) {
+  const yielded = []
+  for await (const value of iter) {
+    if (await existsWith(predicate, value, yielded[symbolIterator]())) continue
+    arrayPush(yielded, value)
+    yield value
+  }
+}
+
+/**
+ * @name unionWith
+ *
+ * @synopsis
+ * Iterable|GeneratorFunction -> SyncSequence
+ * AsyncIterable|AsyncGeneratorFunction -> AsyncSequence
+ * SyncSequence|AsyncSequence -> Sequence
+ *
+ * <T any>unionWith(
+ *   predicate (T, T)=>Promise<boolean>|boolean,
+ * )(values Array<Sequence<T>|T>|T) -> result Promise<Array<T>>|Array<T>
+ *
+ * <T any>unionWith(
+ *   predicate (T, T)=>Promise<boolean>|boolean,
+ * )(values Set<Sequence<T>|T>|T) -> result Promise<Set<T>>|Set<T>
+ *
+ * <T any>unionWith(
+ *   predicate (T, T)=>boolean,
+ * )(values SyncSequence<SyncSequence<T>|T>|T) -> result Iterator<T>
+ *
+ * <T any>unionWith(
+ *   predicate (T, T)=>Promise<boolean>|boolean,
+ * )(values Sequence<Sequence<T>|T>|T) -> result AsyncIterator<T>
+ *
+ * @catchphrase
+ * Unite disparate collections with a predicate
+ *
+ * @description
+ * `unionWith` accepts a binary function `predicate` and any Sequence `values` and returns a flattened collection with all sub items `T` of the Sequence `values` uniq'd by `predicate`. The binary function `predicate` takes two arguments that represent two given sub items `T` and returns an expression that evaluates to true if those two given sub items are the same. A Sequence of values can be an Iterable, GeneratorFunction, AsyncIterable, or AsyncGeneratorFunction. The possible return values of `unionWith` are:
+ *  * Array - for Array `values`
+ *  * Set - for Set `values`
+ *  * Iterator - for Iterables beyond Array and Set, such as GeneratorFunctions
+ *  * AsyncIterator - for AsyncIterables and AsyncGeneratorFunctions
+ */
+const unionWith = predicate => values => {
+  if (values == null) return []
+  const iter = muxConcat(values)
+  if (isArray(values)) return genericUnionWithGeneric(
+    predicate, iter, [], arrayPush)
+  if (isSet(values)) return genericUnionWithGeneric(
+    predicate, iter, new Set(), setAdd)
+  return (iter[symbolIterator]
+    ? iteratorUnionWithIterator(predicate, iter)
+    : asyncIteratorUnionWithAsyncIterator(predicate, iter))
 }
 
 module.exports = unionWith
--- x/unionWith.test.js
+++ x/unionWith.test.js
@@ -1,59 +1,253 @@
+const Mux = require('../monad/Mux')
 const assert = require('assert')
 const unionWith = require('./unionWith')
 
+const { isSequence } = Mux
+
 describe('unionWith', () => {
-  it('create a flattened unique array with uniques given by a binary predicate', async () => {
-    assert.deepEqual(
-      unionWith((a, b) => a.a === b.a)([
-        [{ a: 1 }, { a: 2 }],
-        [{ a: 2 }, { a: 3 }],
-        [{ b: 5 }, { a: 5 }],
-      ]),
-      [{ a: 1 }, { a: 2 }, { a: 3 }, { b: 5 }, { a: 5 }]
-    )
+  describe('<T any>unionWith(predicate (T, T)=>Promise<boolean>|boolean)(values Array<Sequence<T>|T>|T) -> result Promise<Array<T>>|Array<T>', () => {
+    it('create a flattened unique array with uniques given by a binary predicate', async () => {
+      assert.deepEqual(
+        unionWith((a, b) => a.a === b.a)([
+          [{ a: 1 }, { a: 2 }],
+          [{ a: 2 }, { a: 3 }],
+          [{ b: 5 }, { a: 5 }],
+        ]),
+        [{ a: 1 }, { a: 2 }, { a: 3 }, { b: 5 }, { a: 5 }]
+      )
+      assert.deepEqual(
+        unionWith((a, b) => a.a === b.a)([
+          [{ a: 1 }, { a: 1 }],
+          [{ a: 1 }, { a: 1 }],
+          [{ a: 1 }, { a: 2 }],
+        ]),
+        [{ a: 1 }, { a: 2 }]
+      )
+    })
+    it('binary predicate can be async', async () => {
+      assert.deepEqual(
+        await unionWith(async (a, b) => a.a === b.a)([
+          [{ a: 1 }, { a: 2 }],
+          [{ a: 2 }, { a: 3 }],
+          [{ b: 5 }, { a: 5 }],
+        ]),
+        [{ a: 1 }, { a: 2 }, { a: 3 }, { b: 5 }, { a: 5 }]
+      )
+    })
+    it('empty', async () => {
+      assert.deepEqual(
+        unionWith((a, b) => a === b)([]),
+        [],
+      )
+    })
+    it('behavior on varying types', async () => {
+      assert.deepEqual(
+        unionWith((a, b) => a.a === b.a)([
+          [{ a: 1 }, { a: 2 }],
+          [{ a: 2 }, { a: 3 }],
+          [5, 6], // 6.a === undefined === 5.a
+        ]),
+        [{ a: 1 }, { a: 2 }, { a: 3 }, 5]
+      )
+    })
+    it('throws TypeError on unionWith(nonFunction)', async () => {
+      assert.throws(
+        () => unionWith('hey')([1, 2, 3]),
+        new TypeError('predicate is not a function')
+      )
+    })
+    it('unionWith(...)({}) -> Iterator<[{}]>', async () => {
+      const iter = unionWith((a, b) => true)({})
+      const arr = [...iter]
+      assert.deepEqual(arr, [{}])
+    })
   })
-  it('binary predicate can be async', async () => {
-    assert.deepEqual(
-      await unionWith(async (a, b) => a.a === b.a)([
-        [{ a: 1 }, { a: 2 }],
-        [{ a: 2 }, { a: 3 }],
-        [{ b: 5 }, { a: 5 }],
-      ]),
-      [{ a: 1 }, { a: 2 }, { a: 3 }, { b: 5 }, { a: 5 }]
-    )
+
+  describe('<T any>unionWith(predicate (T, T)=>Promise<boolean>|boolean)(values Set<Sequence<T>|T>|T) -> result Promise<Set<T>>|Set<T>', () => {
+    it('predicate () => false; values Set<[1, 2, 3]>; result Set<[1, 2, 3]>', async () => {
+      assert.deepEqual(
+        unionWith(() => false)(new Set([1, 2, 3])),
+        new Set([1, 2, 3]),
+      )
+    })
+    it('predicate () => true; values Set<[1, 2, 3]>; result Set<[1]>', async () => {
+      assert.deepEqual(
+        unionWith(() => true)(new Set([1, 2, 3])),
+        new Set([1]),
+      )
+    })
+    it('predicate async () => false; values Set<[1, 2, 3]>; result Promise<Set<[1, 2, 3]>>', async () => {
+      const p = unionWith(async () => false)(new Set([1, 2, 3]))
+      assert(p instanceof Promise)
+      const result = await p
+      assert.deepEqual(result, new Set([1, 2, 3]))
+    })
+    it('predicate item => item.name; values Set<[{...}, {...}, {...}]>; result Promise|<Set<[{...}, {...}]>>', async () => {
+      const people = new Set([{ name: 'George' }, { name: 'George' }, { name: 'Jim' }])
+      const result = unionWith((personA, personB) => personA.name === personB.name)(people)
+      assert.deepEqual(result, new Set([{ name: 'George' }, { name: 'Jim' }]))
+      {
+        const p = unionWith(async (personA, personB) => personA.name === personB.name)(people)
+        assert(p instanceof Promise)
+        const result = await p
+        assert.deepEqual(result, new Set([{ name: 'George' }, { name: 'Jim' }]))
+      }
+    })
+    it('predicate () => false; values Set<[[1], 2, [3]]>; result Set<[1, 2, 3]>', async () => {
+      assert.deepEqual(
+        unionWith(() => false)(new Set([[1], 2, [3]])),
+        new Set([1, 2, 3]),
+      )
+    })
+    it('predicate () => false; values Set<[1, [2], 3]>; result Set<[1, 2, 3]>', async () => {
+      assert.deepEqual(
+        unionWith(() => false)(new Set([1, [2], 3])),
+        new Set([1, 2, 3]),
+      )
+    })
+    it('predicate () => false; values Set<[1, [2], 3]>; result Set<[1, 2, 3]>', async () => {
+      const asyncNumbers = async function*(){ yield 1; yield 2; yield 3 }
+      assert(
+        unionWith(() => false)([asyncNumbers, asyncNumbers, asyncNumbers]) instanceof Promise
+      )
+      assert.deepEqual(
+        await unionWith(() => false)([asyncNumbers, asyncNumbers, asyncNumbers]),
+        [1, 2, 3, 1, 2, 3, 1, 2, 3],
+      )
+      assert.deepEqual(
+        await unionWith(() => false)(new Set([asyncNumbers, asyncNumbers, asyncNumbers])),
+        new Set([1, 2, 3]),
+      )
+    })
   })
-  it('empty', async () => {
-    assert.deepEqual(
-      unionWith((a, b) => a === b)([]),
-      [],
-    )
+
+  describe('<T any>unionWith(predicate (T, T)=>boolean)(values SyncSequence<SyncSequence<T>|T>|T) -> result Iterator<T>', () => {
+    it('predicate () => false; values [GeneratorFunction, GeneratorFunction, 5, 5, 5]; result [1, 2, 3, 1, 2, 3, 5, 5, 5]', async () => {
+      const numbersGeneratorFunc = function*() { yield 1; yield 2; yield 3 }
+      const subject = unionWith(() => false)([numbersGeneratorFunc, numbersGeneratorFunc, 5, 5, 5])
+      assert.deepEqual(subject, [1, 2, 3, 1, 2, 3, 5, 5, 5])
+    })
+    it('predicate () => false; values GeneratorFunction<>; result Iterator<>', async () => {
+      const numbersGeneratorFunc = function*() { yield 1; yield 2; yield 3 }
+      const subject = unionWith(() => false)(function*(){})
+      const arr = [...subject]
+      assert.deepEqual(arr, [])
+    })
+    it('predicate () => false; values GeneratorFunction<[1, 2, 3]>; result Iterator<[1, 2, 3]>', async () => {
+      const oneGeneratorFunc = function*() { yield 1; yield 1; yield 1 }
+      const subject = unionWith((a, b) => a == b)(oneGeneratorFunc)
+      const arr = [...subject]
+      assert.deepEqual(arr, [1])
+    })
+    it('predicate () => false; values GeneratorFunction<[GeneratorFunction, GeneratorFunction, 5, 5, 5]>; result [1, 2, 3, 1, 2, 3, 5, 5, 5]', async () => {
+      const numbersGeneratorFunc = function*() { yield 1; yield 2; yield 3 }
+      const gen = function*() { yield numbersGeneratorFunc; yield numbersGeneratorFunc; yield* [5, 5, 5] }
+      const subject = unionWith(() => false)(gen)
+      assert(isSequence(subject))
+      const arr = [...subject]
+      assert.deepEqual(arr, [1, 2, 3, 1, 2, 3, 5, 5, 5])
+    })
   })
-  it('behavior on varying types', async () => {
-    assert.deepEqual(
-      unionWith((a, b) => a.a === b.a)([
-        [{ a: 1 }, { a: 2 }],
-        [{ a: 2 }, { a: 3 }],
-        [5, 6],
-      ]),
-      [{ a: 1 }, { a: 2 }, { a: 3 }, 5, 6]
-    )
+
+  describe('<T any>unionWith( predicate (T, T)=>Promise<boolean>|boolean)(values Sequence<Sequence<T>|T>|T) -> result AsyncIterator<T>', () => {
+    it('predicate () => false; values [AsyncGeneratorFunction, GeneratorFunction, 5, 5, 5]; result Promise<[1, 2, 3, 1, 2, 3, 5, 5, 5]>', async () => {
+      const numbersGeneratorFunc = function*() { yield 1; yield 2; yield 3 }
+      const numbersAsyncGeneratorFunc = async function*() { yield 1; yield 2; yield 3 }
+      const gen = async function*() { yield numbersAsyncGeneratorFunc; yield numbersGeneratorFunc; yield* [5, 5, 5] }
+      {
+        const p = unionWith(() => false)([numbersAsyncGeneratorFunc, numbersGeneratorFunc, 5, [5, 5]])
+        assert(p instanceof Promise)
+        const subject = await p
+        const arr = [...subject]
+        assert.deepEqual(arr, [1, 2, 3, 1, 2, 3, 5, 5, 5])
+      }
+      {
+        const p = unionWith(() => Promise.resolve(false))([numbersAsyncGeneratorFunc, numbersGeneratorFunc, 5, [5, 5]])
+        assert(p instanceof Promise)
+        const subject = await p
+        const arr = [...subject]
+        assert.deepEqual(arr, [1, 2, 3, 1, 2, 3, 5, 5, 5])
+      }
+      {
+        const subject = unionWith(() => Promise.resolve(false))(gen)
+        const arr = []
+        for await (const item of subject) arr.push(item)
+        assert.deepEqual(arr, [1, 2, 3, 1, 2, 3, 5, 5, 5])
+      }
+      {
+        const p = unionWith(() => Promise.resolve(false))([async function*(){}])
+        assert(p instanceof Promise)
+        const subject = await p
+        assert.deepEqual(subject, [])
+      }
+    })
+    it('predicate () => false; values AsyncGeneratorFunction<[AsyncGeneratorFunction, GeneratorFunction, 5, 5, 5]>; result AsyncIterator<[1, 2, 3, 1, 2, 3, 5, 5, 5]>', async () => {
+      const numbersGeneratorFunc = function*() { yield 1; yield 2; yield 3 }
+      const numbersAsyncGeneratorFunc = async function*() { yield 1; yield 2; yield 3 }
+      const gen = async function*() { yield numbersAsyncGeneratorFunc; yield numbersGeneratorFunc; yield* [5, 5, 5] }
+      const subject = unionWith(() => false)(gen)
+      const arr = []
+      for await (const item of subject) arr.push(item)
+      assert.deepEqual(arr, [1, 2, 3, 1, 2, 3, 5, 5, 5])
+      {
+        const subject = unionWith(async () => true)(gen)
+        const arr = []
+        for await (const item of subject) arr.push(item)
+        assert.deepEqual(arr, [1])
+      }
+    })
+    it('unionWith(() => false)(emptyAsyncIterator)', async () => {
+      const gen = async function*() {}
+      const subject = unionWith(() => false)(gen)
+      const arr = []
+      for await (const item of subject) arr.push(item)
+      assert.deepEqual(arr, [])
+    })
   })
-  it('throws TypeError on unionWith(nonFunction)', async () => {
-    assert.throws(
-      () => unionWith('hey'),
-      new TypeError('unionWith(f); f is not a binary predicate function')
-    )
-  })
-  it('throws TypeError on unionWith(notBinaryFunction)', async () => {
-    assert.throws(
-      () => unionWith(a => a),
-      new TypeError('unionWith(f); f is not a binary predicate function')
-    )
-  })
-  it('throws TypeError on unionWith(...)(nonArray)', async () => {
-    assert.throws(
-      () => unionWith((a, b) => true)({}),
-      new TypeError('unionWith(...)(x); x is not an Array')
-    )
+
+  describe('misc', () => {
+    it('unionWith(NaN)', async () => {
+      assert(typeof unionWith(NaN) == 'function')
+    })
+    it('unionWith([])', async () => {
+      assert(typeof unionWith([]) == 'function')
+    })
+    it('unionWith({})', async () => {
+      assert(typeof unionWith({}) == 'function')
+    })
+    it('unionWith(undefined)', async () => {
+      assert(typeof unionWith(undefined) == 'function')
+    })
+    it('unionWith(null)', async () => {
+      assert(typeof unionWith(null) == 'function')
+    })
+    it('unionWith(predicate)(NaN)', async () => {
+      const subject = unionWith(() => true)(NaN)
+      assert(isSequence(subject))
+      const arr = [...subject]
+      assert.deepEqual(arr, [NaN])
+    })
+    it('unionWith(predicate)([]); []', async () => {
+      const subject = unionWith(() => true)([])
+      assert.deepEqual(subject, [])
+    })
+    it('unionWith(predicate)({}); Iterator<{}>', async () => {
+      const subject = unionWith(() => true)({})
+      assert(isSequence(subject))
+      const arr = [...subject]
+      assert.deepEqual(arr, [{}])
+    })
+    it('unionWith(predicate)(undefined); []', async () => {
+      const subject = unionWith(() => true)(undefined)
+      assert(isSequence(subject))
+      const arr = [...subject]
+      assert.deepEqual(arr, [])
+    })
+    it('unionWith(predicate)(null); []', async () => {
+      const subject = unionWith(() => true)(null)
+      assert(isSequence(subject))
+      const arr = [...subject]
+      assert.deepEqual(arr, [])
+    })
   })
 })
