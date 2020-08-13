e0db1a76c199132b1cf839be740cf4abf5f60030
Frederic Heem <frederic.heem@gmail.com>
2020-08-13 07:43:46 +0200

    formatter rules

--- x/uniq.js
+++ x/uniq.js
@@ -5,17 +5,15 @@ const { isArray } = Instance;
 const { reduce } = require('../index');
 
 const uniq = (arr) => {
-  if (!isArray(arr)) {
-    throw Error('uniq(arr): arr is not an array');
-  }
+  if (!isArray(arr)) throw Error('uniq(arr): arr is not an array');
+  
   const seenSet = new Set();
   return reduce((acc, value) => {
     if (seenSet.has(value)) {
       return acc;
-    } else {
-      seenSet.add(value);
-      return [...acc, value];
-    }
+    } 
+    seenSet.add(value);
+    return [...acc, value];
   }, [])(arr);
 };
 
