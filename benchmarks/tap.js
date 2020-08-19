63f9eee56cbf7c7316fb1d0dfd9d399be476ee4e
Richard <richytong@gmail.com>
2020-08-18 19:01:18 -0700

    one weird trick

--- benchmarks/tap.js
+++ benchmarks/tap.js
@@ -7,16 +7,26 @@ const R = require('ramda')
  * @name pipe
  *
  * @benchmark
- * tapping: 1e+6: 4.242ms
+ * tapNoopThunk: 1e+6: 4.242ms
+ * tapRegularCallNoopThunk: 1e+6: 4.969ms
  * _tapNoopThunk: 1e+6: 155.716ms
  * RTapNoopThunk: 1e+6: 291.025ms
  */
 
 const noop = () => {}
 
-const tapNoop = tap(noop) // tapping
+const tapNoop = tap(noop)
 
 const tapNoopThunk = () => tapNoop('yo')
+
+tap.regularCall = func => function tapping(input) {
+  const call = func(input)
+  return isPromise(call) ? call.then(() => input) : input
+}
+
+const tapRegularCallNoop = tap.regularCall(noop)
+
+const tapRegularCallNoopThunk = () => tapRegularCallNoop('yo')
 
 const _tapNoop = _.tap(noop)
 
@@ -26,6 +36,8 @@ const RTapNoop = R.tap(noop)
 
 const RTapNoopThunk = () => RTapNoop('yo')
 
+// const func = tapNoopThunk
+// const func = tapRegularCallNoopThunk
 // const func = tapNoopThunk
 // const func = _tapNoopThunk
 // const func = RTapNoopThunk
