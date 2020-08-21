d2e6bae7b0c21d4dea416e8a0d90658a8b2f43a7
Richard <richytong@gmail.com>
2020-08-21 08:26:58 -0700

    bump code versions

--- es.js
+++ es.js
@@ -1,4 +1,4 @@
-/* rubico v1.5.6
+/* rubico v1.5.7
  * https://github.com/a-synchronous/rubico
  * (c) 2019-2020 Richard Tong
  * rubico may be freely distributed under the MIT license.
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
- * rubico v1.5.6
+ * rubico v1.5.7
  * https://github.com/a-synchronous/rubico
  * (c) 2019-2020 Richard Tong
  * rubico may be freely distributed under the MIT license.
