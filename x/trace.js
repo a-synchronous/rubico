e8cebcbfb0145c54f239d1c9eec9d4787b57d405
Richard <richytong@gmail.com>
2020-09-01 11:18:23 -0700

    trace resolver revamp

--- x/trace.js
+++ x/trace.js
@@ -1,5 +1,45 @@
 const { tap } = require('..')
 
-const trace = tap(console.log)
+const consoleLog = console.log
+
+const isPromise = value => value != null && typeof value.then == 'function'
+
+/**
+ * @name funcConcat
+ *
+ * @synopsis
+ * any -> A, any -> B, any -> C
+ *
+ * funcConcat(funcA A=>B, funcB B=>C) -> pipedFunction A=>C
+ */
+const funcConcat = (funcA, funcB) => function piped(...args) {
+  const intermediate = funcA.apply(null, args)
+  return isPromise(intermediate)
+    ? intermediate.then(res => funcB.call(null, res))
+    : funcB.call(null, intermediate)
+}
+
+/**
+ * @name _trace
+ *
+ * @synopsis
+ * _trace(value, args ...any) -> value
+ */
+const _trace = function (value, ...args) {
+  consoleLog(value, ...args)
+  return value
+}
+
+/**
+ * @name trace
+ *
+ * @synopsis
+ */
+const trace = function (value, ...args) {
+  if (typeof value == 'function') {
+    return funcConcat(value, _trace)
+  }
+  return _trace(value, ...args)
+}
 
 module.exports = trace
--- x/trace.test.js
+++ x/trace.test.js
@@ -5,4 +5,20 @@ describe('trace', () => {
   it('logs input to console, returning input', async () => {
     assert.strictEqual(trace('hey'), 'hey')
   })
+  it('lazy trace', async () => {
+    let logged
+    const lazyTrace = trace(value => (logged = `hello ${value}`))
+    assert.equal(typeof lazyTrace, 'function')
+    lazyTrace('world')
+    assert.strictEqual(logged, 'hello world')
+  })
+  it('lazy trace async', async () => {
+    let logged
+    const lazyTrace = trace(async value => (logged = `hello ${value}`))
+    assert.equal(typeof lazyTrace, 'function')
+    const p = lazyTrace('world')
+    assert(p instanceof Promise)
+    await p
+    assert.strictEqual(logged, 'hello world')
+  })
 })
