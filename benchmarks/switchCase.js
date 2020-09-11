e17cda577d0002f6652a84b035b694094a611be0
Richard <richytong@gmail.com>
2020-09-11 10:41:02 -0700

    new switchCase3 benchmarks

--- benchmarks/switchCase.js
+++ benchmarks/switchCase.js
@@ -64,26 +64,102 @@ const switchCase10 = funcConditionalRegularCalls
 
 const switchCase2 = funcs => (...args) => arraySwitchCase(funcs, args, 0)
 
+// argument resolver for curry2
+const curry2ResolveArg0 = (
+  baseFunc, arg1,
+) => function arg0Resolver(arg0) {
+  return baseFunc(arg0, arg1)
+}
+
+// argument resolver for curry2
+const curry2ResolveArg1 = (
+  baseFunc, arg0,
+) => function arg1Resolver(arg1) {
+  return baseFunc(arg0, arg1)
+}
+
+/**
+ * @name curry2
+ *
+ * @synopsis
+ * __ = Symbol('placeholder')
+ *
+ * curry2(
+ *   baseFunc function,
+ *   arg0 __|any,
+ *   arg1 __|any,
+ * ) -> function
+ */
+const curry2 = function (baseFunc, arg0, arg1) {
+  return arg0 == __
+    ? curry2ResolveArg0(baseFunc, arg1)
+    : curry2ResolveArg1(baseFunc, arg0)
+}
+
+const thunkify1 = (func, arg0) => () => func(arg0)
+
+const thunkify3 = (func, arg0, arg1, arg2) => () => func(arg0, arg1, arg2)
+
+const asyncFuncSwitch = async function (funcs, args, funcsIndex) {
+  const lastIndex = funcs.length - 1
+  while ((funcsIndex += 2) < lastIndex) {
+    if (await funcs[funcsIndex](...args)) {
+      return funcs[funcsIndex + 1](...args)
+    }
+  }
+  return funcs[funcsIndex](...args)
+}
+
+const __ = Symbol('placeholder')
+
+const thunkConditional = (
+  boolean, thunkA, thunkB,
+) => boolean ? thunkA() : thunkB()
+
+const switchCase3 = funcs => function funcSwitching(...args) {
+  const lastIndex = funcs.length - 1
+  let funcsIndex = -2
+
+  while ((funcsIndex += 2) < lastIndex) {
+    const shouldReturnNext = funcs[funcsIndex](...args)
+    if (isPromise(shouldReturnNext)) {
+      return shouldReturnNext.then(
+        curry3(
+          thunkConditional,
+          __,
+          thunkify1(
+            curry2(funcApply, funcs[funcsIndex + 1], __),
+            args),
+          thunkify3(asyncFuncSwitch, funcs, args, funcsIndex)))
+    }
+    if (shouldReturnNext) {
+      return funcs[funcsIndex + 1](...args)
+    }
+  }
+  return funcs[funcsIndex](...args)
+}
+
+
 /**
  * @name switchCase
  *
  * @benchmark
- * fiveCases_vanilla: 1e+6: 8.11ms
- * fiveCases_vanillaArgs: 1e+6: 86.009ms
- * fiveCases_switchCaseRubico: 1e+6: 165.871ms
- * fiveCases_switchCaseRubico1: 1e+6: 181.878ms
- * fiveCases_switchCaseRubico10: 1e+6: 165.125ms
- * fiveCases_switchCaseRubico2: 1e+6: 179.485ms
- * fiveMilCases_switchCaseRubico: 5e+0: 260.951ms
- * fiveMilCases_switchCaseRubico1: 5e+0: 318.051ms
- * fiveMilCases_switchCaseRubico10: 5e+0: 270.871ms
- * fiveMilCases_switchCaseRubico2: RangeError: Maximum call stack size exceeded
- * fiveThousandCases_switchCaseRubico: 5e+0: 3.219ms
- * fiveThousandCases_switchCaseRubico1: 5e+0: 9.911ms
- * fiveThousandCases_switchCaseRubico10: 5e+0: 3.372ms
- * fiveThousandCases_switchCaseRubico2: 5e+0: 91.189ms
  *
  * @NOTE Bo5 runs
+ * fiveCases_switchCaseRubico1: 1e+6: 369.264ms
+ * fiveCases_switchCaseRubico10: 1e+6: 314.024ms
+ * fiveCases_switchCaseRubico2: 1e+6: 301.841ms
+ * fiveCases_switchCaseRubico3: 1e+6: 271.723ms
+ *
+ * fiveMilCases_switchCaseRubico1: 5e+0: 326.556ms
+ * fiveMilCases_switchCaseRubico10: 5e+0: 263.211ms
+ * fiveMilCases_switchCaseRubico2: Error: Maximum call stack size exceeded
+ * fiveMilCases_switchCaseRubico3: 5e+0: 262.337ms
+ *
+ * fiveThousandCases_switchCaseRubico1: 5e+2: 63.736ms
+ * fiveThousandCases_switchCaseRubico10: 5e+2: 48.004ms
+ * fiveThousandCases_switchCaseRubico2: 5e+2: 270.643ms
+ * fiveThousandCases_switchCaseRubico3: 5e+2: 25.742ms
  */
 
 {
@@ -116,13 +192,13 @@ const switchCase2 = funcs => (...args) => arraySwitchCase(funcs, args, 0)
 
   const fiveCases_vanillaArgs = (...args) => isOdd(...args) ? odd(...args) : isOdd(...args) ? odd(...args) : even(...args)
 
-  const fiveCases_switchCaseRubico = switchCase(fiveCases)
-
   const fiveCases_switchCaseRubico1 = switchCase1(fiveCases)
 
   const fiveCases_switchCaseRubico10 = switchCase10(fiveCases)
 
   const fiveCases_switchCaseRubico2 = switchCase2(fiveCases)
+
+  const fiveCases_switchCaseRubico3 = switchCase3(fiveCases)
 
   const fiveMilCases = createCases(4_999_999)
 
@@ -134,6 +210,8 @@ const switchCase2 = funcs => (...args) => arraySwitchCase(funcs, args, 0)
 
   const fiveMilCases_switchCaseRubico2 = switchCase2(fiveMilCases)
 
+  const fiveMilCases_switchCaseRubico3 = switchCase3(fiveMilCases)
+
   const fiveThousandCases = createCases(4_999)
 
   const fiveThousandCases_switchCaseRubico = switchCase(fiveThousandCases)
@@ -144,11 +222,11 @@ const switchCase2 = funcs => (...args) => arraySwitchCase(funcs, args, 0)
 
   const fiveThousandCases_switchCaseRubico2 = switchCase2(fiveThousandCases)
 
+  const fiveThousandCases_switchCaseRubico3 = switchCase3(fiveThousandCases)
+
   // timeInLoop('fiveCases_vanilla', 1e6, () => fiveCases_vanilla(2))
 
   // timeInLoop('fiveCases_vanillaArgs', 1e6, () => fiveCases_vanillaArgs(2))
-
-  // timeInLoop('fiveCases_switchCaseRubico', 1e6, () => fiveCases_switchCaseRubico(2))
 
   // timeInLoop('fiveCases_switchCaseRubico1', 1e6, () => fiveCases_switchCaseRubico1(2))
 
@@ -156,19 +234,23 @@ const switchCase2 = funcs => (...args) => arraySwitchCase(funcs, args, 0)
 
   // timeInLoop('fiveCases_switchCaseRubico2', 1e6, () => fiveCases_switchCaseRubico2(2))
 
-  // timeInLoop('fiveMilCases_switchCaseRubico', 5, () => fiveMilCases_switchCaseRubico(2))
+  // console.log(fiveCases_switchCaseRubico3(2))
+
+  // timeInLoop('fiveCases_switchCaseRubico3', 1e6, () => fiveCases_switchCaseRubico3(2))
 
   // timeInLoop('fiveMilCases_switchCaseRubico1', 5, () => fiveMilCases_switchCaseRubico1(2))
 
   // timeInLoop('fiveMilCases_switchCaseRubico10', 5, () => fiveMilCases_switchCaseRubico10(2))
 
-  // timeInLoop('fiveMilCases_switchCaseRubico2', 1e6, () => fiveMilCases_switchCaseRubico2(2))
+  // timeInLoop('fiveMilCases_switchCaseRubico2', 5, () => fiveMilCases_switchCaseRubico2(2))
 
-  timeInLoop('fiveThousandCases_switchCaseRubico', 5, () => fiveThousandCases_switchCaseRubico(2))
+  // timeInLoop('fiveMilCases_switchCaseRubico3', 5, () => fiveMilCases_switchCaseRubico3(2))
 
-  // timeInLoop('fiveThousandCases_switchCaseRubico1', 5, () => fiveThousandCases_switchCaseRubico1(2))
+  // timeInLoop('fiveThousandCases_switchCaseRubico1', 500, () => fiveThousandCases_switchCaseRubico1(2))
 
-  // timeInLoop('fiveThousandCases_switchCaseRubico10', 5, () => fiveThousandCases_switchCaseRubico10(2))
+  // timeInLoop('fiveThousandCases_switchCaseRubico10', 500, () => fiveThousandCases_switchCaseRubico10(2))
 
-  // timeInLoop('fiveThousandCases_switchCaseRubico2', 5, () => fiveThousandCases_switchCaseRubico2(2))
+  // timeInLoop('fiveThousandCases_switchCaseRubico2', 500, () => fiveThousandCases_switchCaseRubico2(2))
+
+  // timeInLoop('fiveThousandCases_switchCaseRubico3', 500, () => fiveThousandCases_switchCaseRubico3(2))
 }
