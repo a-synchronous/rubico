4dfc8b4344271740b66f5ae693bf0a83530487f0
Richard <richytong@gmail.com>
2020-09-11 11:16:11 -0700

    new tryCatch benchmarks

--- benchmarks/tryCatch.js
+++ benchmarks/tryCatch.js
@@ -1,12 +1,80 @@
 const { tryCatch } = require('..')
 const { timeInLoop } = require('../x')
 
+const isPromise = value => value != null && typeof value.then == 'function'
+
+const tryCatch2 = (tryer, catcher) => function tryCatcher(...args) {
+  try {
+    const output = tryer.apply(null, args)
+    return isPromise(output)
+      ? output.catch(err => catcher.call(null, err, ...args))
+      : output
+  } catch (err) {
+    return catcher.call(null, err, ...args)
+  }
+}
+
+// argument resolver for curry3
+const curry3ResolveArg0 = (
+  baseFunc, arg1, arg2,
+) => function arg0Resolver(arg0) {
+  return baseFunc(arg0, arg1, arg2)
+}
+
+// argument resolver for curry3
+const curry3ResolveArg1 = (
+  baseFunc, arg0, arg2,
+) => function arg1Resolver(arg1) {
+  return baseFunc(arg0, arg1, arg2)
+}
+
+// argument resolver for curry3
+const curry3ResolveArg2 = (
+  baseFunc, arg0, arg1,
+) => function arg2Resolver(arg2) {
+  return baseFunc(arg0, arg1, arg2)
+}
+
+/**
+ * @name curry3
+ *
+ * @synopsis
+ * __ = Symbol('placeholder')
+ *
+ * curry3(
+ *   baseFunc function,
+ *   arg0 __|any,
+ *   arg1 __|any,
+ *   arg2 __|any
+ * ) -> function
+ */
+const curry3 = function (baseFunc, arg0, arg1, arg2) {
+  if (arg0 == __) {
+    return curry3ResolveArg0(baseFunc, arg1, arg2)
+  }
+  if (arg1 == __) {
+    return curry3ResolveArg1(baseFunc, arg0, arg2)
+  }
+  return curry3ResolveArg2(baseFunc, arg0, arg1)
+}
+
+const tryCatch3 = (tryer, catcher) => function tryCatcher(...args) {
+  try {
+    const result = tryer(...args)
+    return isPromise(result)
+      ? result.catch(curry3(catcherApply, catcher, __, args))
+      : result
+  } catch (err) {
+    return catcher(err, ...args)
+  }
+}
+
 /**
  * @name tryCatch
  *
  * @benchmark
- * tryCatch1Run: 1e+6: 35.7ms
- * tryCatch2Run: 1e+6: 37.875ms
+ * tryCatch2: 1e+6: 36.369ms
+ * tryCatch3: 1e+6: 17.027ms
  */
 
 {
@@ -14,31 +82,12 @@ const { timeInLoop } = require('../x')
 
   const noop = () => {}
 
-  const tryCatch1 = tryCatch(identity, noop)
-
   const isPromise = value => value != null && typeof value.then == 'function'
 
-  const tryCatchApplyCall = (tryer, catcher) => function tryCatcher(...args) {
-    try {
-      const output = tryer.apply(null, args)
-      return isPromise(output)
-        ? output.catch(err => catcher.call(null, err, ...args))
-        : output
-    } catch (err) {
-      return catcher.call(null, err, ...args)
-    }
-  }
+  // console.log(tryCatch2(identity, noop)('yo'))
+  // console.log(tryCatch3(identity, noop)('yo'))
 
-  const tryCatch2 = tryCatchApplyCall(identity, noop)
+  // timeInLoop('tryCatch2', 1e6, () => tryCatch2(identity, noop)('yo'))
 
-  const tryCatch1Run = () => tryCatch1('yo')
-
-  const tryCatch2Run = () => tryCatch2('yo')
-
-  // const func = tryCatch1Run
-  const func = tryCatch2Run
-
-  console.log('func()', func())
-
-  timeInLoop(func.name, 1e6, func)
+  // timeInLoop('tryCatch3', 1e6, () => tryCatch3(identity, noop)('yo'))
 }
