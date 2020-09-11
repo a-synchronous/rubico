53198bcbd6b40b1d4413c3ba95ce9947d280f121
Richard <richytong@gmail.com>
2020-09-11 11:30:24 -0700

    dont curry assign

--- benchmarks/assign.js
+++ benchmarks/assign.js
@@ -98,13 +98,63 @@ const assign3 = function (funcs) {
   }
 }
 
+// argument resolver for curry2
+const curry2ResolveArg0 = (
+  baseFunc, arg1,
+) => function arg0Resolver(arg0) {
+  return baseFunc(arg0, arg1)
+}
+
+// argument resolver for curry2
+const curry2ResolveArg1 = (
+  baseFunc, arg0,
+) => function arg1Resolver(arg1) {
+  return baseFunc(arg0, arg1)
+}
+
+/**
+ * @name curry2
+ *
+ * @synopsis
+ * __ = Symbol('placeholder')
+ *
+ * curry2(
+ *   baseFunc function,
+ *   arg0 __|any,
+ *   arg1 __|any,
+ * ) -> function
+ */
+const curry2 = function (baseFunc, arg0, arg1) {
+  return arg0 == __
+    ? curry2ResolveArg0(baseFunc, arg1)
+    : curry2ResolveArg1(baseFunc, arg0)
+}
+
+// const objectAssign = Object.assign
+
+const objectAssign = (objA, objB) => ({ ...objA, ...objB })
+
+const assign5 = function (funcs) {
+  const allFuncs = funcObjectAll3(funcs)
+  return function assignment(object) {
+    const result = allFuncs(object)
+    return isPromise(result)
+      ? result.then(curry2(objectAssign, object, __))
+      : ({ ...object, ...result })
+  }
+}
+
 /**
  * @name assign
  *
  * @benchmark
- * assign1: 1e+5: 66.807ms
- * assign2: 1e+5: 59.898ms
- * assign3: 1e+5: 57.537ms
+ * assign1: 1e+5: 67.168ms
+ * assign2: 1e+5: 60.779ms
+ * assign3: 1e+5: 58.094ms
+ * assign5: 1e+5: 59.132ms
+ *
+ * @remarks
+ * currying is slower here
  */
 
 {
@@ -112,10 +162,13 @@ const assign3 = function (funcs) {
   // console.log('assign1', assign1(funcs)({ number: 2 }))
   // console.log('assign2', assign2(funcs)({ number: 2 }))
   // console.log('assign3', assign3(funcs)({ number: 2 }))
+  // console.log('assign5', assign5(funcs)({ number: 2 }))
 
   // timeInLoop('assign1', 1e5, () => assign1(funcs)({ number: 2 }))
 
   // timeInLoop('assign2', 1e5, () => assign2(funcs)({ number: 2 }))
 
   // timeInLoop('assign3', 1e5, () => assign3(funcs)({ number: 2 }))
+
+  // timeInLoop('assign5', 1e5, () => assign5(funcs)({ number: 2 }))
 }
