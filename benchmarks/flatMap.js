1c770a4a54f93f59591ec56ed3792f558edd5082
Richard <richytong@gmail.com>
2020-09-13 21:49:56 -0700

    limited benchmarks

--- benchmarks/flatMap.js
+++ benchmarks/flatMap.js
@@ -5,14 +5,16 @@ const _ = require('lodash')
 const _fp = require('lodash/fp')
 
 /**
- * @name arrayFlatMap
+ * @name competitiveFlatMap
  *
  * @benchmark
  * flatMap(duplicate)(array): 1e+5: 69.215ms
  * R.chain(duplicate, array): 1e+5: 169.668ms
+ * _fp.flatMap(duplicate, array): 1e+5: 85.848ms
  *
  * flatMap(identity)(array): 1e+5: 53.501ms
  * R.chain(identity, array): 1e+5: 160.813ms
+ * _fp.flatMap(identity, array): 1e+5: 81.023ms
  */
 
 {
@@ -24,7 +26,10 @@ const _fp = require('lodash/fp')
 
   // console.log(flatMap(duplicateArray)(numbersArray))
   // console.log(flatMap(identity)(numbersArray))
-  console.log(R.chain(identity, numbersArray))
+  // console.log(R.chain(duplicateArray, numbersArray))
+  // console.log(R.chain(identity, numbersArray))
+  // console.log(_fp.flatMap(duplicateArray, numbersArray))
+  // console.log(_fp.flatMap(identity, numbersArray))
 
   const flatMapDuplicateArray = flatMap(duplicateArray)
 
@@ -32,8 +37,12 @@ const _fp = require('lodash/fp')
 
   // timeInLoop('R.chain(duplicate, array)', 1e5, () => R.chain(duplicateArray, numbersArray))
 
+  // timeInLoop('_fp.flatMap(duplicate, array)', 1e5, () => _fp.flatMap(duplicateArray, numbersArray))
+
   // timeInLoop('flatMap(identity)(array)', 1e5, () => flatMap(identity)(numbersArray))
 
   // timeInLoop('R.chain(identity, array)', 1e5, () => R.chain(identity, numbersArray))
+
+  // timeInLoop('_fp.flatMap(identity, array)', 1e5, () => _fp.flatMap(identity, numbersArray))
 }
 
