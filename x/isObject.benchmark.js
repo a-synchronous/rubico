b2d42b73f269ab5d2e6c76503175e0a9d4836d1a
Richard <richytong@gmail.com>
2020-10-01 16:08:50 -0700

    isObject refresh benchmarks

--- x/isObject.benchmark.js
+++ x/isObject.benchmark.js
@@ -8,9 +8,13 @@ const _ = require('lodash')
  * @benchmark
  * isObject({}): 1e+7: 14.796ms
  * _.isObject({}): 1e+7: 14.601ms
+ *
+ * richytong 2020-10-01
+ * isObject({}): 1e+7: 14.69ms
+ * _.isObject({}): 1e+7: 14.536ms
  */
 
-// timeInLoop('isObject({})', 1e7, () => isObject({}))
+timeInLoop('isObject({})', 1e7, () => isObject({}))
 
 const _isObject = _.isObject
 
