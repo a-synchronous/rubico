39968d09522f2f545ec09862b2a4dbb6144fd176
Richard <richytong@gmail.com>
2020-10-01 16:06:49 -0700

    new isObject

--- x/isObject.js
+++ x/isObject.js
@@ -1,5 +1,28 @@
-const Instance = require('../monad/Instance')
-
-const { isObject } = Instance
+/**
+ * @name isObject
+ *
+ * @synopsis
+ * ```coffeescript [specscript]
+ * isObject(value any) -> boolean
+ * ```
+ *
+ * @description
+ * Determine whether a value is a direct `Object`.
+ *
+ * ```javascript [node]
+ * console.log(
+ *   isObject({ a: 1, b: 2, c: 3 }),
+ * ) // true
+ *
+ * console.log(
+ *   isObject('hey'),
+ * ) // false
+ *
+ * console.log(
+ *   isObject(new Set([1, 2, 3])),
+ * ) // false
+ * ```
+ */
+const isObject = value => value != null && value.constructor == Object
 
 module.exports = isObject
