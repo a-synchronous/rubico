ed08223b81d84c7675d1337a44a354fa749c2dd2
Richard <richytong@gmail.com>
2020-10-01 19:06:52 -0700

    fresh unionWith benchmarks

--- x/unionWith.benchmark.js
+++ x/unionWith.benchmark.js
@@ -8,6 +8,10 @@ const _ = require('lodash')
  * @benchmark
  * unionWith((a, b) => a.a == b.a)(nestedObjects): 1e+5: 95.553ms
  * _.unionWith((a, b) => a.a == b.a)(nestedObjects): 1e+5: 100.701ms
+ *
+ * richytong 2020-10-01:
+ * unionWith((a, b) => a.a == b.a)(nestedObjects): 1e+5: 38.952ms
+ * _.unionWith((a, b) => a.a == b.a)(nestedObjects): 1e+5: 101.171ms
  */
 
 const nestedObjects = [
@@ -21,6 +25,9 @@ const subject = unionWith((a, b) => a.a == b.a)
 const _unionWith = _.unionWith
 
 const _subject = _unionWith((a, b) => a.a == b.a)
+
+// console.log(subject(nestedObjects))
+// console.log(_unionWith(...nestedObjects, (a, b) => a.a == b.a))
 
 // timeInLoop('unionWith((a, b) => a.a == b.a)(nestedObjects)', 1e5, () => { subject(nestedObjects) })
 
