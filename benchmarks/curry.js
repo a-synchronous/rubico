fd0221eacc8ec1ddd39643978511a7af309ae602
Richard <richytong@gmail.com>
2020-10-16 09:11:37 -0700

    curry unary benchmarks

--- benchmarks/curry.js
+++ benchmarks/curry.js
@@ -6,6 +6,8 @@ const curry2 = require('../_internal/curry2')
 const curry3 = require('../_internal/curry3')
 const curry4 = require('../_internal/curry4')
 
+const { __ } = rubico
+
 /**
  * @name curryRace
  *
@@ -34,4 +36,20 @@ const add6 = (a, b, c, d, e, f) => a + b + c + d + e + f
 // timeInLoop('rubico.curry', 1e5, () => rubico.curry(add6)(1, 2, rubico.__)(3)(4, rubico.__, 6)(5))
 // timeInLoop('R.curry', 1e5, () => R.curry(add6)(1, 2, R.__)(3)(4, R.__, 6)(5))
 
-const { __ } = rubico
+/**
+ * @name curryRaceUnary
+ *
+ * @benchmark
+ * rubico.curry: 1e+5: 60.554ms
+ * R.curry: 1e+5: 106.163ms
+ *
+ * rubico.curry: 1e+5: 45.475ms
+ * R.curry: 1e+5: 66.493ms
+ */
+const identity = value => value
+
+// console.log(rubico.curry(identity)(__)(1)) // 1
+// console.log(R.curry(identity)(R.__)(1)) // 1
+
+// timeInLoop('rubico.curry', 1e5, () => rubico.curry(identity)(__)(1))
+// timeInLoop('R.curry', 1e5, () => R.curry(identity)(R.__)(1))
