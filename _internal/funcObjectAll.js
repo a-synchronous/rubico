0cde6e5bf40777873ab1949988eccb7f5740f16c
Richard <richytong@gmail.com>
2020-10-27 15:24:00 -0700

    fix: stop using promiseObjectAll

--- _internal/funcObjectAll.js
+++ _internal/funcObjectAll.js
@@ -1,5 +1,9 @@
+const always = require('./always')
 const isPromise = require('./isPromise')
-const promiseObjectAll = require('./promiseObjectAll')
+const promiseAll = require('./promiseAll')
+const __ = require('./placeholder')
+const curry3 = require('./curry3')
+const objectSet = require('./objectSet')
 
 /**
  * @name funcObjectAll
@@ -15,16 +19,16 @@ const promiseObjectAll = require('./promiseObjectAll')
  * Concurrently execute the same arguments for each function of an object of functions, returning an object of results.
  */
 const funcObjectAll = funcs => function objectAllFuncs(...args) {
-  const result = {}
-  let isAsync = false
+  const result = {}, promises = []
   for (const key in funcs) {
     const resultItem = funcs[key](...args)
-    if (!isAsync && isPromise(resultItem)) {
-      isAsync = true
+    if (isPromise(resultItem)) {
+      promises.push(resultItem.then(curry3(objectSet, result, key, __)))
+    } else {
+      result[key] = resultItem
     }
-    result[key] = resultItem
   }
-  return isAsync ? promiseObjectAll(result) : result
+  return promises.length == 0 ? result : promiseAll(promises).then(always(result))
 }
 
 module.exports = funcObjectAll
