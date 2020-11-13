2fd503e4368077ebce3c37a01384666cffb75bd4
Richard <richytong@gmail.com>
2020-11-12 23:21:58 -0800

    amend isObject test

--- x/isObject.test.js
+++ x/isObject.test.js
@@ -4,8 +4,8 @@ const assert = require('assert')
 describe('isObject', () => {
   it('is Object?', async () => {
     assert.strictEqual(isObject({}), true)
-    assert.strictEqual(isObject([]), false)
-    assert.strictEqual(isObject(Object), false)
+    assert.strictEqual(isObject([]), true)
+    assert.strictEqual(isObject(Object), true)
     assert.strictEqual(isObject(''), false)
     assert.strictEqual(isObject(String()), false)
     assert.strictEqual(isObject(Object()), true)
