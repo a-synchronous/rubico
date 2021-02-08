1cb239a1867da836c619230914ca8de7cf3a52a9
Richard <richytong@gmail.com>
2021-02-08 11:21:37 -0800

    bufferLoading does not always load at least one item

--- _internal/FlatMappingAsyncIterator.js
+++ _internal/FlatMappingAsyncIterator.js
@@ -48,30 +48,39 @@ const FlatMappingAsyncIterator = function (asyncIterator, flatMapper) {
       }
       const { value, done } = await asyncIterator.next()
       if (done) {
-        if (promises.size > 0) {
+        while (promises.size > 0) {
           await promiseRace(promises)
-          return { value: buffer.shift(), done: false } // bufferLoading always loads at least one item
+          if (buffer.length > 0) {
+            return { value: buffer.shift(), done: false }
+          }
         }
         return { value: undefined, done: true }
       }
 
-      // this will always load at least one item
       const monad = flatMapper(value)
       if (isPromise(monad)) {
         const bufferLoading = monad.then(
           curryArgs3(genericReduce, __, arrayPush, buffer))
         const promise = bufferLoading.then(() => promises.delete(promise))
         promises.add(promise)
-        await promiseRace(promises)
       } else {
         const bufferLoading = genericReduce([monad], arrayPush, buffer)
+        if (buffer.length > 0) {
+          return { value: buffer.shift(), done: false }
+        }
         if (isPromise(bufferLoading)) {
           const promise = bufferLoading.then(() => promises.delete(promise))
           promises.add(promise)
-          await promiseRace(promises)
         }
       }
-      return { value: buffer.shift(), done: false } // bufferLoading always loads at least one item
+
+      while (promises.size > 0) {
+        await promiseRace(promises)
+        if (buffer.length > 0) {
+          return { value: buffer.shift(), done: false }
+        }
+      }
+      return { value: undefined, done: true }
     },
   }
 }
