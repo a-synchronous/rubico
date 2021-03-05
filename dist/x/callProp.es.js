7f017bfcdac4c72054b9aa5b2cdd0a3d1e75eb8e
Richard <richytong@gmail.com>
2021-03-05 09:54:53 -0800

    callProp distribution (current version)

---
+++ dist/x/callProp.es.js
@@ -0,0 +1,12 @@
+/**
+ * rubico v1.6.21
+ * https://github.com/a-synchronous/rubico
+ * (c) 2019-2020 Richard Tong
+ * rubico may be freely distributed under the MIT license.
+ */
+
+const callProp = (property, ...args) => function callingProp(object) {
+  return object[property](...args)
+}
+
+export default callProp
