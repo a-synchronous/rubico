6732dc52dccabb3214f90710135e229167d06103
Richard <richytong@gmail.com>
2021-03-05 09:58:10 -0800

    make obvious docs string

--- x/callProp.js
+++ x/callProp.js
@@ -7,11 +7,13 @@
  * ```
  *
  * @description
+ * Calls a property on an object with arguments.
+ *
  * ```javascript [playground]
  * import callProp from 'https://unpkg.com/rubico/dist/x/callProp.es.js'
  *
  * const priceRoundedDown = callProp('toFixed', 2)(5.992)
- * console.log('priceRoundedDown', priceRoundedDown) // 5.99
+ * console.log('priceRoundedDown', priceRoundedDown) // '5.99'
  * ```
  */
 const callProp = (property, ...args) => function callingProp(object) {
