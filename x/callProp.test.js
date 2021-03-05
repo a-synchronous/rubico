f61b6a1dba593b07b06612c03c08d53156417298
Richard <richytong@gmail.com>
2021-03-05 10:03:58 -0800

    no loss of precision

--- x/callProp.test.js
+++ x/callProp.test.js
@@ -3,7 +3,7 @@ const callProp = require('./callProp')
 
 describe('callProp', () => {
   it('calls a property on an object with arguments', async () => {
-    const roundedDown = callProp('toFixed', 2)(5.99222222222222222222)
+    const roundedDown = callProp('toFixed', 2)(5.992)
     assert.strictEqual(roundedDown, '5.99')
     assert.strictEqual(callProp('toString')(1), '1')
   })
