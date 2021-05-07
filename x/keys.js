7900b62ad6ea4043d9ccfb058b64881c9a51a837
Richard <richytong@gmail.com>
2021-05-07 14:48:52 -0700

    more docs for keys

--- x/keys.js
+++ x/keys.js
@@ -5,9 +5,7 @@ const objectKeys = require('../_internal/objectKeys')
  *
  * @synopsis
  * ```coffeescript [specscript]
- * keys<T>(
- *   mapLike String<T>|Array<T>|Set<T>|Map<any=>T>|Object<T>,
- * ) -> Array<T>
+ * keys(value string|Array|Set|Map|object) -> Array<key number|string>
  * ```
  *
  * @description
@@ -17,7 +15,7 @@ const objectKeys = require('../_internal/objectKeys')
  * import keys from 'https://unpkg.com/rubico/dist/x/keys.es.js'
  *
  * console.log(keys({ a: 1, b: 2, c: 3 })) // ['a', 'b', 'c']
- * console.log(keys('abc')) // [0, 1, 2]
+ * console.log(keys(['hello', 'world'])) // [0, 1]
  * console.log(keys(new Map([['hello', 1], ['world', 2]]))) // ['hello', 'world']
  * ```
  *
