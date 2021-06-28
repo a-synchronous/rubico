312ce20c2775be79b7de6838c30c4a36ea6b0730
Frederic Heem <frederic.heem@gmail.com>
2021-06-28 23:17:22 +0200

    node 12 friendly

--- x/defaultsDeep.js
+++ x/defaultsDeep.js
@@ -22,7 +22,7 @@ const arrayDefaultsDeepFromArray = function (array, defaultArray) {
       result[index] = arrayDefaultsDeepFromArray(item, defaultItem)
     } else if (item == null) {
       result[index] = defaultItem
-    } else if (item?.constructor == Object && defaultItem?.constructor == Object) {
+    } else if (item.constructor == Object && defaultItem && defaultItem.constructor == Object) {
       result[index] = objectDefaultsDeepFromObject(item, defaultItem)
     } else {
       result[index] = item
@@ -51,7 +51,7 @@ const objectDefaultsDeepFromObject = function (object, defaultObject) {
       result[key] = arrayDefaultsDeepFromArray(item, defaultItem)
     } else if (item == null) {
       result[key] = defaultItem
-    } else if (item?.constructor == Object && defaultItem?.constructor == Object) {
+    } else if (item.constructor == Object && defaultItem && defaultItem.constructor == Object) {
       result[key] = objectDefaultsDeepFromObject(item, defaultItem)
     } else {
       result[key] = item
