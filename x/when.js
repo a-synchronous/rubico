d49cb85f6f99647eaeb71905fe1cc4cfa0002ea1
Richard <richytong@gmail.com>
2021-07-19 08:27:24 -0700

    when handle async

--- x/when.js
+++ x/when.js
@@ -1,29 +1,49 @@
+const isPromise = require('../_internal/isPromise')
+const always = require('../_internal/always')
+const __ = require('../_internal/placeholder')
+const thunkify1 = require('../_internal/thunkify1')
+const curry3 = require('../_internal/curry3')
+const thunkConditional = require('../_internal/thunkConditional')
+
 /**
  * @name when
  *
+ * @synopsis
+ * ```coffeescript [specscript]
+ * when(
+ *   predicate any=>Promise|boolean,
+ *   func function,
+ * )(object Object) -> Promise|any
+ * ```
+ *
  * @description
- * Execute a function and return the result when a condition is true, otherwise return the original object
+ * Execute a function and return the result when a condition is true, otherwise return the original object.
  *
  * ```javascript [playground]
  * import when from 'https://unpkg.com/rubico/dist/x/when.es.js'
  *
- * const isEven = (num) => num % 2 === 0;
-
- * const doubleIfEven = when(
- *  isEven,
- *  (num) => num * 2
- * );
+ * const isEven = num => num % 2 === 0
+ * const doubleIfEven = when(isEven, num => num * 2)
  *
  * console.log(doubleIfEven(100)) // 200
  * console.log(doubleIfEven(101)) // 101
  * ```
  */
 
-const when = (predicate, fn) => function when(object) {
-    if (predicate(object)) {
-      return fn(object)
-    }
-    return object;
-  };
+const when = (predicate, func) => function whenFunc(object) {
+  const predication = predicate(object)
+  if (isPromise(predication)) {
+    return predication.then(curry3(
+      thunkConditional,
+      __,
+      thunkify1(func, object),
+      always(object),
+    ))
+  }
+  if (predication) {
+    return func(object)
+  }
+  return object
+}
 
 module.exports = when
--- x/when.test.js
+++ x/when.test.js
@@ -1,13 +1,21 @@
-const assert = require('assert');
-const when = require('./when');
+const assert = require('assert')
+const when = require('./when')
 
-const isEven = num => num % 2 === 0;
+const isEven = num => num % 2 === 0
 
 describe('when', () => {
   it('happy path', async () => {
-    const doubleIfEven = when(isEven, num => num * 2);
+    const doubleIfEven = when(isEven, num => num * 2)
 
-    assert.strictEqual(doubleIfEven(100), 200);
-    assert.strictEqual(doubleIfEven(101), 101);
-  });
-});
+    assert.strictEqual(doubleIfEven(100), 200)
+    assert.strictEqual(doubleIfEven(101), 101)
+  })
+
+  it('async happy path', async () => {
+    const asyncIsEven = async num => num % 2 == 0
+    const asyncDoubleIfEven = when(asyncIsEven, num => num * 2)
+
+    assert.strictEqual(await asyncDoubleIfEven(100), 200)
+    assert.strictEqual(await asyncDoubleIfEven(101), 101)
+  })
+})
