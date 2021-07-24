499484a8d26109520882bc744855fe5f34813e73
Frederic Heem <frederic.heem@gmail.com>
2021-07-24 14:21:25 -0300

    unless

---
+++ x/unless.js
@@ -0,0 +1,48 @@
+const isPromise = require('../_internal/isPromise');
+const always = require('../_internal/always');
+const __ = require('../_internal/placeholder');
+const thunkify1 = require('../_internal/thunkify1');
+const curry3 = require('../_internal/curry3');
+const thunkConditional = require('../_internal/thunkConditional');
+
+/**
+ * @name unless
+ *
+ * @synopsis
+ * ```coffeescript [specscript]
+ * when(
+ *   predicate any=>Promise|boolean,
+ *   func function,
+ * )(value any) -> Promise|any
+ * ```
+ *
+ * @description
+ * Execute a function and return the result unless a condition is true, otherwise return the original value.
+ *
+ * ```javascript [playground]
+ * import unless from 'https://unpkg.com/rubico/dist/x/unless.es.js'
+ *
+ * const isEven = num => num % 2 === 0
+ * const doubleIfEven = unless(isEven, num => num * 2)
+ *
+ * console.log(doubleIfEven(100)) // 100
+ * console.log(doubleIfEven(101)) // 202
+ * ```
+ *
+ * @since 1.7.2
+ */
+
+const unless = (predicate, func) => function unlessFunc(value) {
+    const predication = predicate(value)
+    if (isPromise(predication)) {
+      return predication.then(
+        curry3(thunkConditional, __, always(value), thunkify1(func, value))
+      )
+    }
+    if (!predication) {
+      return func(value)
+    }
+    return value
+  }
+
+module.exports = unless;
