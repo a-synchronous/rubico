fcee10de5add37b92d73f29bc6c27ff08f76fc7c
Richard <richytong@gmail.com>
2022-03-21 17:24:30 -0700

    update isDeepEqual docs to mention function arguments

--- x/isDeepEqual.js
+++ x/isDeepEqual.js
@@ -137,6 +137,18 @@ const areValuesDeepEqual = function (left, right) {
  *   isDeepEqual({ a: 1, b: 2, c: [3] }, { a: 1, b: 2, c: [5] }),
  * ) // false
  * ```
+ *
+ * When passed a resolver function as the left or right argument or resolvers as both arguments, returns a function that resolves the value by the resolver before performing the deep equal comparison.
+ *
+ * ```javascript [playground]
+ * import isDeepEqual from 'https://unpkg.com/rubico/dist/x/isDeepEqual.es.js'
+ *
+ * const isPropADeepEqualTo123Array = isDeepEqual(object => object.a, [1, 2, 3])
+ *
+ * console.log(
+ *   isPropADeepEqualTo123Array({ a: [1, 2, 3] }),
+ * ) // true
+ * ```
  */
 const isDeepEqual = function (left, right) {
   const isLeftResolver = typeof left == 'function',
