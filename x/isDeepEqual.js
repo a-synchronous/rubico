7d5cbdd7a9a9db3579e33a3c9ec7f25ca4b0de4e
Richard <richytong@gmail.com>
2022-03-21 17:06:38 -0700

    isDeepEqual takes functions as arguments, returning a partially applied function

--- x/isDeepEqual.js
+++ x/isDeepEqual.js
@@ -1,4 +1,9 @@
+const __ = require('../_internal/placeholder')
 const isArray = require('../_internal/isArray')
+const isPromise = require('../_internal/isPromise')
+const promiseAll = require('../_internal/promiseAll')
+const spread2 = require('../_internal/spread2')
+const curry2 = require('../_internal/curry2')
 const objectKeysLength = require('../_internal/objectKeysLength')
 const symbolIterator = require('../_internal/symbolIterator')
 const sameValueZero = require('../_internal/sameValueZero')
@@ -66,34 +71,14 @@ const areArraysDeepEqual = function (leftArray, rightArray) {
 }
 
 /**
- * @name isDeepEqual
+ * @name areValuesDeepEqual
  *
  * @synopsis
  * ```coffeescript [specscript]
- * Nested<T> = Array<Array<T>|Object<T>|Iterable<T>|T>|Object<Array<T>|Object<T>|Iterable<T>|T>
- *
- * var left Nested,
- *   right Nested
- *
- * isDeepEqual(left, right) -> boolean
- * ```
- *
- * @description
- * Check two values for deep [SameValueZero](http://ecma-international.org/ecma-262/7.0/#sec-samevaluezero) equality.
- *
- * ```javascript [playground]
- * import isDeepEqual from 'https://unpkg.com/rubico/dist/x/isDeepEqual.es.js'
- *
- * console.log(
- *   isDeepEqual({ a: 1, b: 2, c: [3] }, { a: 1, b: 2, c: [3] }),
- * ) // true
- *
- * console.log(
- *   isDeepEqual({ a: 1, b: 2, c: [3] }, { a: 1, b: 2, c: [5] }),
- * ) // false
+ * areValuesDeepEqual(left any, right any) -> boolean
  * ```
  */
-const isDeepEqual = function (left, right) {
+const areValuesDeepEqual = function (left, right) {
   const isLeftArray = isArray(left),
     isRightArray = isArray(right)
   if (isLeftArray || isRightArray) {
@@ -123,6 +108,80 @@ const isDeepEqual = function (left, right) {
       && areObjectsDeepEqual(left, right)
   }
   return sameValueZero(left, right)
+}
+
+/**
+ * @name isDeepEqual
+ *
+ * @synopsis
+ * ```coffeescript [specscript]
+ * Nested<T> = Array<Array<T>|Object<T>|Iterable<T>|T>|Object<Array<T>|Object<T>|Iterable<T>|T>
+ *
+ * var left Nested,
+ *   right Nested
+ *
+ * isDeepEqual(left, right) -> boolean
+ * ```
+ *
+ * @description
+ * Check two values for deep [SameValueZero](http://ecma-international.org/ecma-262/7.0/#sec-samevaluezero) equality.
+ *
+ * ```javascript [playground]
+ * import isDeepEqual from 'https://unpkg.com/rubico/dist/x/isDeepEqual.es.js'
+ *
+ * console.log(
+ *   isDeepEqual({ a: 1, b: 2, c: [3] }, { a: 1, b: 2, c: [3] }),
+ * ) // true
+ *
+ * console.log(
+ *   isDeepEqual({ a: 1, b: 2, c: [3] }, { a: 1, b: 2, c: [5] }),
+ * ) // false
+ * ```
+ */
+const isDeepEqual = function (left, right) {
+  const isLeftResolver = typeof left == 'function',
+    isRightResolver = typeof right == 'function'
+  if (isLeftResolver && isRightResolver) {
+    return function isDeepEqualBy(value) {
+      const leftValue = left(value),
+        rightValue = right(value)
+      const isLeftPromise = isPromise(leftValue),
+        isRightPromise = isPromise(rightValue)
+      if (isLeftPromise && isRightPromise) {
+        return promiseAll([
+          leftValue,
+          rightValue,
+        ]).then(spread2(areValuesDeepEqual))
+      }
+      if (isLeftPromise) {
+        return leftValue.then(curry2(areValuesDeepEqual, __, rightValue))
+      }
+      if (isRightPromise) {
+        return rightValue.then(curry2(areValuesDeepEqual, leftValue, __))
+      }
+      return areValuesDeepEqual(leftValue, rightValue)
+    }
+  }
+
+  if (isLeftResolver) {
+    return function isDeepEqualBy(value) {
+      const leftValue = left(value)
+      return isPromise(leftValue)
+        ? leftValue.then(curry2(areValuesDeepEqual, __, right))
+        : areValuesDeepEqual(leftValue, right)
+    }
+  }
+
+  if (isRightResolver) {
+    return function isDeepEqualBy(value) {
+      const rightValue = right(value)
+      return isPromise(rightValue)
+        ? rightValue.then(curry2(areValuesDeepEqual, left, __))
+        : areValuesDeepEqual(left, rightValue)
+    }
+  }
+
+  return areValuesDeepEqual(left, right)
 }
 
 module.exports = isDeepEqual
