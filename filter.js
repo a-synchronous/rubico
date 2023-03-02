0a009a54b094be95ad8ae60b369f8806f1bb352b
Richard <richytong@gmail.com>
2023-03-02 14:30:59 -0800

    revised docs for filter

--- filter.js
+++ filter.js
@@ -102,67 +102,86 @@ const _filter = function (value, predicate) {
 /**
  * @name filter
  *
- * @catchphrase
- * exclude items by predicate
- *
  * @synopsis
  * ```coffeescript [specscript]
- * filter(
- *   arrayPredicate (value any, index number, array Array)=>Promise|boolean
- * )(array Array) -> filteredArray Promise|Array
+ * arrayPredicate (value any, index number, array Array)=>Promise|boolean
  *
- * filter(
- *   objectPredicate (value any, key string, object Object)=>Promise|boolean
- * )(object Object) -> filteredObject Promise|Object
+ * filter(arrayPredicate)(array Array) -> filteredArray Promise|Array
+ * filter(array Array, arrayPredicate) -> filteredArray Promise|Array
  *
- * filter(
- *   setPredicate (value any, value, set Set)=>Promise|boolean
- * )(set Set) -> filteredSet Promise|Set
+ * objectPredicate (value any, key string, object Object)=>Promise|boolean
  *
- * filter(
- *   mapPredicate (value any, key any, map Map)=>Promise|boolean
- * )(map Map) -> filteredMap Promise|Map
+ * filter(objectPredicate)(object Object) -> filteredObject Promise|Object
+ * filter(object Object, objectPredicate) -> filteredObject Promise|Object
  *
- * filter(
- *   predicate (value any)=>Promise|boolean
- * )(generatorFunction GeneratorFunction) -> filteringGeneratorFunction GeneratorFunction
+ * setPredicate (value any, value, set Set)=>Promise|boolean
  *
- * filter(
- *   predicate (value any)=>Promise|boolean
- * )(asyncGeneratorFunction AsyncGeneratorFunction) -> filteringAsyncGeneratorFunction AsyncGeneratorFunction
+ * filter(setPredicate)(set Set) -> filteredSet Promise|Set
+ * filter(set Set, setPredicate) -> filteredSet Promise|Set
  *
- * filter(
- *   predicate (value any)=>Promise|boolean
- * )(reducer Reducer) -> filteringReducer Reducer
+ * mapPredicate (value any, key any, map Map)=>Promise|boolean
+ *
+ * filter(mapPredicate)(map Map) -> filteredMap Promise|Map
+ * filter(map Map, mapPredicate) -> filteredMap Promise|Map
  * ```
  *
  * @description
- * Filter out items from a filterable based on the results of their concurrent executions with a predicate. `filter` recognizes several vanilla JavaScript types as filterables.
+ * Filter out items from a collection based on the results of their concurrent executions with a synchronous or asynchronous predicate function. `filter` accepts the following collections:
  *
- *  * `Array` - filter array items by predicate
- *  * `Object` - filter object values by predicate
- *  * `Set` - filter Set values by predicate
- *  * `Map` - filter Map values by predicate (not entries)
- *  * `Iterator` - skip iterations by predicate
- *  * `AsyncIterator` - skip async iterations by predicate
+ *  * `Array`
+ *  * `Object`
+ *  * `Set`
+ *  * `Map`
+ *  * `Iterator`/`Generator`
+ *  * `AsyncIterator`/`AsyncGenerator`
+ *  * Special types with a `.filter` method `{ filter: function }`
+ *
+ * With arrays (type `Array`), `filter` applies the predicate function to each item of the array, returning a new array containing only the items that tested truthy by the predicate. The order of the items is preserved.
  *
  * ```javascript [playground]
  * const isOdd = number => number % 2 == 1
  *
- * console.log(
- *   filter(isOdd)([1, 2, 3, 4, 5]),
- * ) // [1, 3, 5]
+ * const array = [1, 2, 3, 4, 5]
  *
- * console.log(
- *   filter(isOdd)({ a: 1, b: 2, c: 3, d: 4, e: 5 }),
- * ) // { a: 1, c: 3, e: 5 }
- *
- * console.log(
- *   filter(isOdd)(new Set([1, 2, 3, 4, 5])),
- * ) // Set { 1, 3, 5 }
+ * console.log(filter(isOdd)(array)) // [1, 3, 5]
+ * console.log(filter(array, isOdd)) // [1, 3, 5]
  * ```
  *
- * Passing a generator function to `filter` in filterable position returns a generator function that produces filtering generators; all values that are normally yielded by a generator that test falsy by the predicate are skipped by a filtering generator.
+ * With objects (type `Object`), `filter` applies the predicate function to each value of the object, returning a new object containing only the values that tested truthy by the predicate.
+ *
+ * ```javascript [playground]
+ * const isOdd = number => number % 2 == 1
+ *
+ * const obj = { a: 1, b: 2, c: 3, d: 4, e: 5 }
+ *
+ * console.log(filter(isOdd)(obj)) // { a: 1, c: 3, e: 5 }
+ * console.log(filter(obj, isOdd)) // { a: 1, c: 3, e: 5 }
+ * ```
+ *
+ * With sets (type `Set`), `filter` applies the predicate function to each item in the set, returning a new set containing only the items that tested truthy by the predicate.
+ *
+ * ```javascript [playground]
+ * const isOdd = number => number % 2 == 1
+ *
+ * const set = new Set([1, 2, 3, 4, 5])
+ *
+ * console.log(filter(isOdd)(set)) // Set { 1, 3, 5 }
+ * console.log(filter(set, isOdd)) // Set { 1, 3, 5 }
+ * ```
+ *
+ * With maps (type `Map`), `filter` applies the predicate function to the value of each entry of the map, returning a new map containing only the entries where the values tested truthy by the predicate. The order of the entries are preserved.
+ *
+ * ```javascript [playground]
+ * const isOdd = number => number % 2 == 1
+ *
+ * const obj = { a: 1, b: 2, c: 3, d: 4, e: 5 }
+ * const m = new Map([['a', 1], ['b', 2], ['c', 3], ['d', 4], ['e', 5]])
+ *
+ * console.log(filter(isOdd)(m)) // Map(3) { 'a' => 1, 'c' => 3, 'e' => 5 }
+ * console.log(filter(m, isOdd)) // Map(3) { 'a' => 1, 'c' => 3, 'e' => 5 }
+ * ```
+ *
+ * With iterators (type `Iterator`) or generators (type `Generator`), `filter` returns a lazily filtered iterator/generator; all values that are normally yielded by the iterator/generator that test falsy by the predicate function are skipped by the lazily filtered iterator/generator.
  *
  * ```javascript [playground]
  * const isOdd = number => number % 2 == 1
@@ -171,24 +190,32 @@ const _filter = function (value, predicate) {
  *   yield 1; yield 2; yield 3; yield 4; yield 5
  * }
  *
- * const oddNumbersGeneratorFunction = filter(isOdd)(numbersGeneratorFunction)
+ * const numbersGenerator = numbersGeneratorFunction()
+ * const oddNumbersGenerator = filter(numbersGeneratorFunction(), isOdd)
+ * const oddNumbersGenerator2 = filter(isOdd)(numbersGeneratorFunction())
  *
- * for (const number of oddNumbersGeneratorFunction()) {
+ * for (const number of numbersGenerator) {
  *   console.log(number) // 1
+ *                       // 2
  *                       // 3
+ *                       // 4
  *                       // 5
  * }
- *
- * const oddNumbersGenerator = filter(isOdd)(numbersGeneratorFunction())
  *
  * for (const number of oddNumbersGenerator) {
  *   console.log(number) // 1
  *                       // 3
  *                       // 5
  * }
+ *
+ * for (const number of oddNumbersGenerator2) {
+ *   console.log(number) // 1
+ *                       // 3
+ *                       // 5
+ * }
  * ```
  *
- * In a similar vein to generator functions, passing an async generator function in filterable position creates an async generator function of filtered async generators. Each filtered async generator skips async iterations with values that test falsy by the predicate.
+ * With asyncIterators (type `AsyncIterator`) or asyncGenerators (type `AsyncGenerator`), `filter` returns a lazily filtered asyncIterator/asyncGenerator; all values that are normally yielded by the asyncIterator/asyncGenerator that test falsy by the predicate function are skipped by the lazily filtered asyncIterator/asyncGenerator.
  *
  * ```javascript [playground]
  * const asyncIsOdd = async number => number % 2 == 1
@@ -197,45 +224,31 @@ const _filter = function (value, predicate) {
  *   yield 1; yield 2; yield 3; yield 4; yield 5
  * }
  *
- * const asyncOddNumbersGeneratorFunction = filter(asyncIsOdd)(asyncNumbersGeneratorFunction)
+ * const asyncNumbersGenerator = asyncNumbersGeneratorFunction()
  *
- * for await (const number of asyncOddNumbersGeneratorFunction()) {
+ * const asyncOddNumbersGenerator = filter(asyncNumbersGeneratorFunction(), asyncIsOdd)
+ *
+ * const asyncOddNumbersGenerator2 = filter(asyncIsOdd)(asyncNumbersGeneratorFunction())
+ *
+ * for await (const number of asyncNumbersGenerator) {
  *   console.log(number) // 1
+ *                       // 2
  *                       // 3
+ *                       // 4
  *                       // 5
  * }
- *
- * const asyncOddNumbersGenerator = filter(asyncIsOdd)(asyncNumbersGeneratorFunction())
  *
  * for await (const number of asyncOddNumbersGenerator) {
  *   console.log(number) // 1
  *                       // 3
  *                       // 5
  * }
- * ```
  *
- * A reducer in filterable position creates a filtering reducer - one that skips items of the reducer's reducing operation if they test falsy by the predicate. It is possible to use an asynchronous predicate when filtering a reducer, however the implementation of `reduce` must support asynchronous operations. This library provides such an implementation as `reduce`.
- *
- * ```javascript [playground]
- * const isOdd = number => number % 2 == 1
- *
- * const concat = (array, item) => array.concat(item)
- *
- * const concatOddNumbers = filter(isOdd)(concat)
- *
- * console.log(
- *   [1, 2, 3, 4, 5].reduce(concatOddNumbers, []),
- * ) // [1, 3, 5]
- * ```
- *
- * `filter`, when passed a single argument before the predicate function, treats that argument as the value to be filtered.
- *
- * ```javascript [playground]
- * const numbers = [1, 2, 3]
- *
- * const odds = filter(numbers, number => number % 2 == 1)
- *
- * console.log(odds) // [1, 3]
+ * for await (const number of asyncOddNumbersGenerator2) {
+ *   console.log(number) // 1
+ *                       // 3
+ *                       // 5
+ * }
  * ```
  *
  * @execution concurrent
@@ -256,12 +269,9 @@ const filter = function (...args) {
  *
  * @synopsis
  * ```coffeescript [specscript]
- * var T any,
- *   index number,
- *   array Array<T>,
- *   indexedPredicate (T, index, array)=>Promise|boolean
- *
- * filter.withIndex(indexedPredicate)(array) -> Array<T>
+ * filter.withIndex(
+ *   indexedPredicate (item any, index number, array Array)=>Promise|boolean
+ * )(array Array) -> result Array
  * ```
  *
  * @description
@@ -269,12 +279,15 @@ const filter = function (...args) {
  *
  * ```javascript [playground]
  * const uniq = filter.withIndex(
- *   (item, index, array) => item !== array[index + 1])
+ *   (item, index, array) => item !== array[index + 1]
+ * )
  *
  * console.log(
  *   uniq([1, 1, 1, 2, 2, 2, 3, 3, 3]),
  * ) // [1, 2, 3]
  * ```
+ *
+ * @DEPRECATED
  *
  * @execution concurrent
  */
