abd0e1044457aa844ea4465cb514f40bf609fd8e
Richard <richytong@gmail.com>
2023-03-03 14:40:28 -0800

    revised docs for and

--- and.js
+++ and.js
@@ -68,13 +68,27 @@ const asyncArePredicatesAllTruthy = async function (predicates, point, index) {
  *
  * @synopsis
  * ```coffeescript [specscript]
+ * and(values Array<boolean>) -> result boolean
+ *
  * and(
- *   predicates Array<predicate function|nonfunction>
- * )(point any) -> Promise|boolean
+ *   predicatesOrValues Array<function|boolean>
+ * )(value any) -> result Promise|boolean
  * ```
  *
  * @description
- * Test an array of predicates or nonfunction values concurrently against a single input, returning true if all test truthy.
+ * Tests an array of boolean values, returning true if all boolean values are truthy.
+ *
+ * ```javascript [playground]
+ * const oneIsLessThanThree = 1 < 3
+ * const twoIsGreaterThanOne = 2 > 1
+ * const threeIsEqualToThree = 3 === 3
+ *
+ * console.log(
+ *   and([oneIsLessThanThree, twoIsGreaterThanOne, threeIsEqualToThree]),
+ * ) // true
+ * ```
+ *
+ * If any values in the array are synchronous or asynchronous predicate functions, `and` takes another argument to test concurrently against the predicate functions, returning true if all array values and resolved values from the predicates are truthy.
  *
  * ```javascript [playground]
  * const isOdd = number => number % 2 == 1
@@ -85,14 +99,6 @@ const asyncArePredicatesAllTruthy = async function (predicates, point, index) {
  *
  * console.log(
  *   and([isOdd, isPositive, isLessThan3])(1),
- * ) // true
- * ```
- *
- * `and` behaves eagerly if provided only nonfunction values
- *
- * ```javascript [playground]
- * console.log(
- *   and([true, true, true]),
  * ) // true
  * ```
  *
