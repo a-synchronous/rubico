dceecc017f978cc38d137dd310ee9d711910ef38
Richard <richytong@gmail.com>
2023-03-03 15:08:31 -0800

    revised docs for thunkify

--- thunkify.js
+++ thunkify.js
@@ -3,14 +3,11 @@
  *
  * @synopsis
  * ```coffeescript [specscript]
- * var func function,
- *   args ...any
- *
- * thunkify(func, ...args) -> thunk ()=>func(...args)
+ * thunkify(func function, ...args) -> thunk ()=>func(...args)
  * ```
  *
  * @description
- * Create a thunk from a function and any number of arguments. A thunk is a function that takes no arguments - the computation it represents has already been "thunk" with given function and arguments.
+ * Create a thunk function from another function and any number of arguments. The thunk function takes no arguments, and when called, executes the other function with the provided arguments. The other function is said to be "thunkified".
  *
  * ```javascript [playground]
  * const add = (a, b) => a + b
