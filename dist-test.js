09c77d1ee3211b49a27f3b51e15bbb88660690f7
Richard <richytong@gmail.com>
2023-06-11 19:12:57 -0700

    dist-test makes some assertions

--- dist-test.js
+++ dist-test.js
@@ -1,22 +1,35 @@
+const assert = require('assert')
 const findAllFilePaths = require('./_internal/findAllFilePaths')
 
 const distTest = async function main() {
   const filePaths = await findAllFilePaths(`${__dirname}/dist`)
-  filePaths.push('index.js')
-  filePaths.push('es.js')
+  filePaths.push(`${__dirname}/index.js`)
+  filePaths.push(`${__dirname}/es.js`)
 
   for (const path of filePaths) {
-    if (path.includes('.es') || path.endsWith('.mjs')) {
+    if (
+      path.includes('.es')
+      || path.endsWith('.mjs')
+      || path.includes('es.js')
+    ) {
       // TODO
-    } else if (path == 'es.js') {
-      // TODO
-    } else if (path == 'index.js') {
+    }
+    else {
       console.log('Testing', path)
-      const rubico = require('.')
-      console.log(rubico)
-    } else {
-      console.log('Testing', path)
-      require(path)
+      const required = require(path)
+
+      if (
+        path.includes('rubico.js')
+        || path.includes('rubico.min.js')
+        || path.includes('index.js')
+      ) {
+        assert.equal(Object.keys(required).length, 31)
+      } else if (
+        path.includes('Transducer.js')
+        || path.includes('Transducer.min.js')
+      ) {
+        assert.equal(Object.keys(required).length, 5)
+      }
     }
   }
 }
