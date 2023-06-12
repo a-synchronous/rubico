537815a0d0a35976cb075e4359b489f07f6a4264
Richard <richytong@gmail.com>
2023-06-11 17:25:31 -0700

    dist-test tests index.js and es.js

--- dist-test.js
+++ dist-test.js
@@ -2,13 +2,22 @@ const findAllFilePaths = require('./_internal/findAllFilePaths')
 
 const distTest = async function main() {
   const filePaths = await findAllFilePaths(`${__dirname}/dist`)
+  filePaths.push('index.js')
+  filePaths.push('es.js')
+
   for (const path of filePaths) {
     if (path.includes('.es') || path.endsWith('.mjs')) {
       // TODO
-      continue
+    } else if (path == 'es.js') {
+      // TODO
+    } else if (path == 'index.js') {
+      console.log('Testing', path)
+      const rubico = require('.')
+      console.log(rubico)
+    } else {
+      console.log('Testing', path)
+      require(path)
     }
-    console.log('Testing', path)
-    require(path)
   }
 }
 
